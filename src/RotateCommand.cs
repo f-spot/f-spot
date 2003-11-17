b53b114a40aef659a1a3a032cf3cac5dcf0ab72a
Ettore Perazzoli <ettore@src.gnome.org>
2003-11-17 02:46:52 +0000

    New.
    * src/RotateCommand.cs: New.

---
+++ src/RotateCommand.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using Gtk;
+
+public class RotateCommand {
+
+	private Gtk.Window parent_window;
+
+	public RotateCommand (Gtk.Window parent_window)
+	{
+		this.parent_window = parent_window;
+	}
+
+	public enum RotationType {
+		Clockwise,
+		Counterclockwise,
+	}
+
+	public bool Execute (RotationType rotation, Photo [] photos)
+	{
+		int count = 0;
+		foreach (Photo p in photos) {
+			foreach (uint version_id in p.VersionIds) {
+				string original_path = p.GetVersionPath (version_id);
+				string temporary_path = original_path + ".tmp";	// FIXME make it unique
+
+				// FIXME exception
+
+				if (rotation == RotationType.Clockwise)
+					JpegUtils.Transform (original_path, temporary_path, JpegUtils.TransformType.Rotate90);
+				else
+					JpegUtils.Transform (original_path, temporary_path, JpegUtils.TransformType.Rotate270);
+
+				// FIXME way to do this atomically in .NET?  I think Move() raises an exception
+				// if the destination path points to an existing file.
+				File.Delete (original_path);
+				File.Move (temporary_path, original_path);
+
+				PhotoStore.GenerateThumbnail (original_path);
+			}
+
+			count ++;
+		}
+
+		return true;
+	}
+}
