6b2c658ab9c385c5d7f3779a6a325a3fdd6034cf
Larry Ewing <lewing@ximian.com>
2004-06-10 21:25:10 +0000

    add new util file. Mostly code from Miguel for the popup items. `
    2004-06-10  Larry Ewing  <lewing@ximian.com>
    
    	* src/Util.cs: add new util file.  Mostly code from Miguel for the
    	popup items.
    `

---
+++ src/Util.cs
@@ -0,0 +1,124 @@
+//
+// Utility functions.
+//
+// Miguel de Icaza (miguel@ximian.com).
+//
+// (C) 2002 Ximian, Inc.
+//
+//
+
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Collections;
+using System.Text;
+using System;
+
+class Util {
+
+#if WIN32
+	public const char DirSep = '\\';
+#else
+	public const char DirSep = '/';
+#endif
+
+}
+
+class Semaphore {
+	int count = 0;
+	
+	public Semaphore ()
+	{ }
+
+	public void Down ()
+	{
+		lock (this){
+			while (count <= 0){
+				Monitor.Wait (this, Timeout.Infinite);
+			}
+			count--;
+		}
+	}
+
+	public void Up ()
+	{
+		lock (this){
+			count++;
+			Monitor.Pulse (this);
+		}
+	}
+}
+
+class UriList : ArrayList {
+	public UriList (Photo [] photos) {
+		foreach (Photo p in photos) {
+			Uri uri;
+			try {
+				uri = new Uri ("file://" + p.DefaultVersionPath);
+			} catch {
+				continue;
+			}
+			Add (uri);
+		}
+	}
+
+	public UriList (string data) {
+		string [] items = data.Split ('\n');
+
+		foreach (String i in items) {
+			if (!i.StartsWith ("#")) {
+				Uri uri;
+				
+				try {
+					uri = new Uri (i);
+				} catch {
+					continue;
+				}
+				Add (uri);
+			}
+		}
+	}
+
+	public override string ToString () {
+		StringBuilder list = new StringBuilder ();
+
+		foreach (Uri uri in this) {
+			if (uri == null)
+				break;
+
+			list.Append (uri.ToString () + "\r\n");
+		}
+
+		return list.ToString ();
+	}
+
+	public string [] ToPaths () {
+		foreach (Uri uri in this) {
+			
+		}
+		return null;
+	}
+}
+
+class GtkUtil {
+	public static void MakeMenuItem (Gtk.Menu menu, string l, EventHandler e)
+	{
+		MakeMenuItem (menu, l, e, true);
+	}
+	
+	public static void MakeMenuItem (Gtk.Menu menu, string l, EventHandler e, bool enabled)
+	{
+		Gtk.MenuItem i = new Gtk.MenuItem (l);
+		i.Activated += e;
+                i.Sensitive = enabled;
+		
+		menu.Append (i);
+		i.Show ();
+	}
+	
+	public static void MakeMenuSeparator (Gtk.Menu menu)
+	{
+		Gtk.SeparatorMenuItem i = new Gtk.SeparatorMenuItem ();
+		menu.Append (i);
+		i.Show ();
+	}
+}
