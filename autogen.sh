615c92d5b32a0aafa6757baae78a7e5374760369
Larry Ewing <lewing@src.gnome.org>
2004-08-16 21:27:03 +0000

    make this work with automake-1.8

--- autogen.sh
+++ autogen.sh
@@ -4,19 +4,16 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="F-Spot"
+PKG_NAME="f-spot"
 
-(test -f $srcdir/configure.in \
-  && test -f $srcdir/ChangeLog \
-  && test -d $srcdir/src) || {
+(test -f $srcdir/configure.in) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level F-Spot directory"
+    echo " top-level $PKG_NAME directory"
     exit 1
 }
-
 
 which gnome-autogen.sh || {
     echo "You need to install gnome-common from the GNOME CVS"
     exit 1
 }
-USE_GNOME2_MACROS=1 . gnome-autogen.sh
+REQUIRED_AUTOMAKE_VERSION=1.7 USE_GNOME2_MACROS=1 ACLOCAL_FLAGS="$ACLOCAL_FLAGS -I $srcdir/m4" . gnome-autogen.sh
