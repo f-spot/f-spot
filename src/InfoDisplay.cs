1c07ad980809185bf8ec3cb863cb74df987186af
Larry Ewing <lewing@novell.com>
2004-09-23 20:03:19 +0000

    render the exif thumbnail, fix colortype.
    2004-09-23  Larry Ewing  <lewing@novell.com>
    
    	* src/InfoDisplay.cs: render the exif thumbnail, fix colortype.

--- src/InfoDisplay.cs
+++ src/InfoDisplay.cs
@@ -20,8 +20,12 @@ namespace FSpot {
 				if (exif_info != null)
 					exif_info.Dispose ();
 
-				exif_info = new ExifData (photo.DefaultVersionPath);
-				exif_info.Assemble ();
+				if (photo != null) {
+					exif_info = new ExifData (photo.DefaultVersionPath);
+					exif_info.Assemble ();
+				} else {
+					exif_info = null;
+				}
 				this.Update ();
 			}
 		}
@@ -37,30 +41,42 @@ namespace FSpot {
 			Byte r = (byte)(color.Red / 256);
 			Byte b = (byte)(color.Blue / 256);
 			Byte g = (byte)(color.Green / 256);
-			string value =  r.ToString ("x") + g.ToString ("x") + b.ToString ("x");
+			string value =  r.ToString ("x2") + g.ToString ("x2") + b.ToString ("x2");
 			System.Console.WriteLine (value);
 			return value;
 		}
 
+		protected override void OnUrlRequested (string url, Gtk.HTMLStream stream)
+		{
+			if (url == "exif:thumbnail") {
+				byte [] data = exif_info.Data;
+				
+				stream.Write (data, data.Length);
+				stream.Close (Gtk.HTMLStreamStatus.Ok);
+			}
+		}
+
 		private void Update ()
 		{
-			ExifTag [] tags = exif_info.Tags;
 			Gtk.HTMLStream stream = this.Begin ("text/html; charset=utf-8");
 			
 			string bg = Color (this.Style.Background (Gtk.StateType.Active));
 			string fg = Color (this.Style.Foreground (Gtk.StateType.Active));
 
-			stream.Write ("<table width=100% cellspacing=0 cellpadding=3>");
-			System.Console.WriteLine (tags.Length);
-			foreach (ExifTag tag in tags) {
-				stream.Write ("<tr><td bgcolor=\""+ bg + "\"><font color=\"" + fg + "\">");
-				stream.Write (ExifUtil.GetTagTitle (tag));
-				stream.Write ("</font></td><td>");
-				if (exif_info.LookupString (tag) != "")
-					stream.Write (exif_info.LookupString (tag));
-				stream.Write ("</td><tr>");
+			if (exif_info != null) {
+				stream.Write ("<table width=100% cellspacing=0 cellpadding=3>");
+				stream.Write ("<tr><td colspan=2 align=\"center\" bgcolor=\"" + bg + "\"><img center src=\"exif:thumbnail\"></td></tr>");
+				foreach (ExifTag tag in exif_info.Tags) {
+					stream.Write ("<tr><td bgcolor=\""+ bg + "\"><font color=\"" + fg + "\">");
+					stream.Write (ExifUtil.GetTagTitle (tag));
+					stream.Write ("</font></td><td>");
+					if (exif_info.LookupString (tag) != "")
+						stream.Write (exif_info.LookupString (tag));
+					stream.Write ("</td><tr>");
+				}
+				stream.Write ("</table>");
 			}
-			stream.Write ("</table>");
+
 			End (stream, Gtk.HTMLStreamStatus.Ok);
 		}
 	}
