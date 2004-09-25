9c90e7716c26dd8f46aa5586e3ee08f8029df2e4
Larry Ewing <lewing@novell.com>
2004-09-25 20:21:52 +0000

    rewrite this so that we don't have to sort the full query. Make set glass
    2004-09-25  Larry Ewing  <lewing@novell.com>
    
    	* src/DirectoryAdaptor.cs: rewrite this so that we don't have to
    	sort the full query.  Make set glass actually do the right thing.

--- src/DirectoryAdaptor.cs
+++ src/DirectoryAdaptor.cs
@@ -4,24 +4,47 @@ using System.Collections;
 namespace FSpot {
 	public class DirectoryAdaptor : GroupAdaptor {
 		public PhotoQuery query;
-		public ArrayList dirs = new ArrayList ();
+		System.Collections.DictionaryEntry [] dirs;
 
+		// FIXME store the Photo.Id list here not just the count
+		private class Group : IComparer {
+			public int Count = 1;
 
-		struct DirInfo {
-			public string Path;
-			public int Count;
-		}
+			public int Compare (object obj1, object obj2)
+			{
+				// FIXME use a real exception
+				if (obj1 is DictionaryEntry && obj2 is DictionaryEntry)
+					return Compare ((DictionaryEntry)obj1, (DictionaryEntry)obj2);
+				else 
+					throw new Exception ("I can't compare that");
+				
+				
+			}
 		
+			private static int Compare (DictionaryEntry de1, DictionaryEntry de2)
+			{
+				return string.Compare ((string)de1.Key, (string)de2.Key);
+			}
+		}
+
+
 		public override event GlassSetHandler GlassSet;
 
 		public override void SetGlass (int group)
 		{
-			Console.WriteLine ("Selected Path {0}", ((DirInfo)dirs [group]).Path);
-			
+			if (group < 0 || group > dirs.Length)
+				return;
+
+			Console.WriteLine ("Selected Path {0}", dirs [group].Key);
+	
 			int item = 0;
 			int i = 0;
-			while (i < group) {
-				item += ((DirInfo)dirs [i++]).Count;
+			while (i < query.Photos.Length) {
+				if (query.Photos [i].DirectoryPath == (string)dirs [group].Key) {
+					item = i;
+					break;
+				}
+				i++;
 			}
 
 			if (GlassSet != null)
@@ -30,13 +53,12 @@ namespace FSpot {
 
 		public override int Count ()
 		{
-			return dirs.Count;
+			return dirs.Length;
 		}
 
 		public override string GlassLabel (int item)
 		{
-			DirInfo info = (DirInfo)dirs[item];
-			return info.Path;
+			return (string)dirs [item].Key;
 		}
 		
 		public override string TickLabel (int item)
@@ -46,8 +68,7 @@ namespace FSpot {
 
 		public override int Value (int item) 
 		{
-			DirInfo info = (DirInfo)dirs[item];	
-			return info.Count;
+			return ((DirectoryAdaptor.Group)dirs [item].Value).Count;
 		}	
 
 		private void HandleReload (PhotoQuery query)
@@ -58,24 +79,23 @@ namespace FSpot {
 		
 		public override void Reload () 
 		{
-			dirs.Clear ();
+			System.Collections.Hashtable ht = new System.Collections.Hashtable ();
 			Photo [] photos = query.Store.Query (null, null);
 			
-			Array.Sort (photos, new Photo.CompareDirectory ());
-			Array.Sort (query.Photos, new Photo.CompareDirectory ());
-
-			DirInfo info = new DirInfo ();
 			foreach (Photo p in photos) {
-				string dir = p.DirectoryPath;
-				if (dir != info.Path) {
-					if (info.Path != null)
-						dirs.Add (info);
-
-					info.Path = dir;
-					info.Count = 0;
-				}
-				info.Count += 1;
+				if (ht.Contains (p.DirectoryPath)) {
+					DirectoryAdaptor.Group group = (DirectoryAdaptor.Group) ht [p.DirectoryPath];
+					group.Count += 1;
+				} else 
+					ht [p.DirectoryPath] = new DirectoryAdaptor.Group ();
 			}
+			
+			Console.WriteLine ("Count = {0}", ht.Count);
+			dirs = new System.Collections.DictionaryEntry [ht.Count];
+			ht.CopyTo (dirs, 0);
+			
+			Array.Sort (dirs, new DirectoryAdaptor.Group ());
+			Array.Sort (query.Photos, new Photo.CompareDirectory ());
 		}
 
 		public DirectoryAdaptor (PhotoQuery query) {
