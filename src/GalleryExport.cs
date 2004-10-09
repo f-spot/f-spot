4c3c23bbb27e43d0dd36955aed96fb4c0f5cfec1
Larry Ewing <lewing@src.gnome.org>
2004-10-09 18:54:45 +0000

    missed this

--- src/GalleryExport.cs
+++ src/GalleryExport.cs
@@ -1,11 +1,74 @@
 namespace FSpot {
-	public class GalleryAdd {
-		public GalleryAdd (System.Collections.ArrayList list) {
-			gallery_list = list;
+	public class GalleryAccount {
+		public GalleryAccount (string name, string url, string username, string password)
+		{
+			this.name = name;
+			this.username = username;
+			this.password = password;
+			this.url = url;
+		}
+		
+		public GalleryRemote.Gallery Connect ()
+		{
+			try {
+				gallery = new GalleryRemote.Gallery (url);
+				gallery.Login (username, password);
+				connected = true;
+				return gallery;
+			} catch (System.Exception e) {
+				// FIXME handle this login exception better
+				System.Console.WriteLine (e);
+				return null;
+			}
+		}
 
+		private bool connected;
+		public bool Connected {
+			get {
+				return connected;
+			}
+		}
+
+		GalleryRemote.Gallery gallery;
+		public GalleryRemote.Gallery Gallery {
+			get {
+				return gallery;
+			}
+		}
+
+		string name;
+		public string Name {
+			get {
+				return name;
+			}
+		}
+		
+		string url;
+		public string Url {
+			get {
+				return url;
+			}
+		}
+
+		string username;
+		public string Username {
+			get {
+				return username;
+			}
+		}
+
+      		string password;
+		public string Password {
+			get {
+				return password;
+			}
+		}
+	}
+	
+	public class GalleryAdd {
+		public GalleryAdd (GalleryExport export) {
 			Glade.XML xml = new Glade.XML (null, "f-spot.glade", "gallery_add_dialog", null);
 			xml.Autoconnect (this);
-			
 		}
 
 		private void HandleChanged (object sender, System.EventArgs args)
@@ -21,32 +84,24 @@ namespace FSpot {
 				add_button.Sensitive = true;
 		}
 
-		private HandleAdd (object sender, System.EventArgs args)
+		private void HandleAdd (object sender, System.EventArgs args)
 		{
-
-			try {
-				GalleryRemote.Gallery gallery = new GalleryRemote.Gallery (name, url);
-				gallery.Login (username, password);
-				gallery_list.Add (gallery);
-			} catch (System.Exception e) {
-				System.Console.WriteLine (e);
-				// FIXME Set an Error Label
-				add_button.Sensitive = false;
-			}
+			GalleryAccount account = new GalleryAccount (name, url, username, password);
+			export.AddAccount (account);
 		}
 		
-		private HandleCancel (object sender, System.EventArgs args)
+		private void HandleCancel (object sender, System.EventArgs args)
 		{
 			Dialog.Destroy ();
 		}
 
 		public Gtk.Dialog Dialog {
 			get {
-				gallery_add_dialog;
+				return gallery_add_dialog;
 			}
 		}
 
-		private System.Collections.ArrayList gallery_list;
+		GalleryExport export;
 
 		private string name;
 		private string url;
@@ -66,13 +121,13 @@ namespace FSpot {
 	}
 	
 	public class GalleryAddAlbum {
-		public AlbumAdd (GalleryRemote gallery)
+		public GalleryAddAlbum (GalleryRemote.Gallery gallery)
 		{
 		}
 
 		public Gtk.Dialog Dialog {
 			get {
-				dialog;
+				return dialog;
 			}
 		}
 		
@@ -83,30 +138,44 @@ namespace FSpot {
 	public class GalleryExport {
 		public GalleryExport (Photo [] photos) 
 		{
+			this.photos = photos;
+			
 			Glade.XML xml = new Glade.XML (null, "f-spot.glade", "gallery_export_dialog", null);
 			xml.Autoconnect (this);
 
-			export_gallery_dialog.DefaultResponse = ResponseType.Ok;
+			LoadAccounts ();
 
-			LoadGalleryList ();
-			PopulateGalleryOptionMenu ();
+			Gtk.ResponseType response = (Gtk.ResponseType) gallery_export_dialog.Run ();
 
-			ResponseType response = (ResponseType) gallery_export_dialog.Run ();
+			if (response == Gtk.ResponseType.Cancel)
+				return;
 
-			scale = scale_check.Toggled;
-			broswer = browser_check.Toggled;
-			meta = meta_check.Toggled;
+			scale = scale_check.Active;
+			browser = browser_check.Active;
+			meta = meta_check.Active;
 
-			if (gallery) { 
-				album = gallery.Albums [album_optionmenu.History]; 
+			if (gallery != null) { 
+				System.Console.WriteLine ("history = {0}", album_optionmenu.History);
+				album = (GalleryRemote.Album) gallery.Albums [System.Math.Max (0, album_optionmenu.History)]; 
+				photo_index = 0;
+
+				System.Threading.Thread t = new System.Threading.Thread (new System.Threading.ThreadStart (this.Upload));
+				t.Name = "Uploading Pictures";
+				
+				progress_dialog = new FSpot.ThreadProgressDialog (t, photos.Length);
+				progress_dialog.Start ();
 			}
 		}
-		
+
 		private bool scale;
 		private bool browser;
 		private bool meta;
+
+		Photo [] photos;
+		int photo_index;
+		FSpot.ThreadProgressDialog progress_dialog;
 		
-		System.Collections.ArrayList gallery_list;
+		System.Collections.ArrayList accounts;
 		private GalleryRemote.Gallery gallery;
 		private GalleryRemote.Album album;
 
@@ -116,7 +185,7 @@ namespace FSpot {
 
 		// Widgets
 		[Glade.Widget] Gtk.Dialog gallery_export_dialog;
-
+		
 		[Glade.Widget] Gtk.OptionMenu gallery_optionmenu;
 		[Glade.Widget] Gtk.OptionMenu album_optionmenu;
 		
@@ -127,45 +196,96 @@ namespace FSpot {
 		[Glade.Widget] Gtk.CheckButton scale_check;
 		[Glade.Widget] Gtk.CheckButton meta_check;
 
+		[Glade.Widget] Gtk.Button album_button;
+		[Glade.Widget] Gtk.Button add_button;
+
 		public Gtk.Dialog Dialog {
 			get {
 				return gallery_export_dialog;
 			}
 		}
 
-		private void LoadGalleryList ()
+		private void Upload ()
 		{
-			gallery_list = new System.Collections.ArrayList ();
+			try {
+				System.Console.WriteLine ("Starting upload");
+				for (; photo_index < photos.Length; photo_index++) {
+					Photo photo = photos [photo_index];
+
+					System.Console.WriteLine ("uploading {0}", photo_index);
+
+					progress_dialog.Message = System.String.Format ("Uploading picture \"{0}\"", photo.Name);
+					progress_dialog.Fraction = photo_index / (double) photos.Length;
+					photo_index++;
+
+					progress_dialog.ProgressText = System.String.Format ("{0} of {1}", photo_index, photos.Length);
+					album.Add (photo);
+				}
+
+				progress_dialog.Message = ("Done Sending Photos");
+				progress_dialog.Fraction = 1.0;
+				progress_dialog.ProgressText = "Upload Complete";
+			} catch (System.Exception e) {
+				progress_dialog.Message = e.ToString ();
+				progress_dialog.ProgressText = "Error Uploading To Gallery";
+			}
+		}
+		
+		private void LoadAccounts ()
+		{
+			accounts = new System.Collections.ArrayList ();
+			AddAccount (new GalleryAccount ("People", "http://discord.no-ip.com/people/gallery/gallery_remote2.php", "admin", "batman"));
+			AddAccount (new GalleryAccount ("Joe", "http://discord.no-ip.com/people/gallery/gallery_remote2.php", "billy", "batman"));
+
 		}
 
-		public bool AddGallery (GalleryRemote.Gallery gallery)
+		public void AddAccount (GalleryAccount account)
 		{
-			gallery_list.Add (galler
+			accounts.Add (account);
+			PopulateGalleryOptionMenu ();
 		}
 
 		private void PopulateGalleryOptionMenu ()
 		{
-
 			Gtk.Menu menu = new Gtk.Menu ();
 			
-			if (gallery_list.Count == 0) {
-				Gtk.MenuItem item = new Gtk.MenuItem ("(None)");
+			if (accounts.Count == 0) {
+				Gtk.MenuItem item = new Gtk.MenuItem ("(No Gallery)");
+				menu.Append (item);
 				gallery_optionmenu.Sensitive = false;
 			} else {
-				foreach (GalleryRemote.Gallery gallery in gallery_list) {
-					Gtk.MenuItem item = new MenuItem (label_builder.ToString ());
-					menu.Append (item);					
+				foreach (GalleryAccount account in accounts) {
+					Gtk.MenuItem item = new Gtk.MenuItem (account.Name);
+					menu.Append (item);		
 				}
 				gallery_optionmenu.Sensitive = true;
 			}
+
+			menu.ShowAll ();
+			gallery_optionmenu.Menu = menu;
 		}
 
-		private void PopulateAlbumOptionMenu (GalleryRemote gallery)
+		private void HandleAccountSelected (object sender, System.EventArgs args)
+		{
+			if (accounts.Count != 0) {
+				GalleryAccount account = (GalleryAccount) accounts [gallery_optionmenu.History];
+				if (!account.Connected)
+					account.Connect ();
+				
+				gallery = account.Gallery;
+
+				PopulateAlbumOptionMenu (gallery);
+			}
+		}
+
+		private void PopulateAlbumOptionMenu (GalleryRemote.Gallery gallery)
 		{
 			System.Collections.ArrayList albums = null;
 			
-			if (gallery != null)
+			if (gallery != null) {
+				gallery.FetchAlbums ();
 				albums = gallery.Albums;
+			}
 
 			Gtk.Menu menu = new Gtk.Menu ();
 			
@@ -187,10 +307,14 @@ namespace FSpot {
 					}
 					label_builder.Append (album.Name);
 
-					Gtk.MenuItem item = new MenuItem (label_builder.ToString ());
+					Gtk.MenuItem item = new Gtk.MenuItem (label_builder.ToString ());
 					menu.Append (item);
+			
+				        GalleryRemote.AlbumPermission add_permission = album.Perms & GalleryRemote.AlbumPermission.Add;
+
+					if (add_permission == 0)
+						item.Sensitive = false;
 				}
-				
 				album_optionmenu.Sensitive = true;
 				album_button.Sensitive = true;
 			}
@@ -201,7 +325,7 @@ namespace FSpot {
 		
 		public void HandleAddGallery (object sender, System.EventArgs args)
 		{
-			gallery_add = new GalleryAdd (gallery_list);
+			gallery_add = new GalleryAdd (this);
 		}
 
 		public void HandleAddAlbum (object sender, System.EventArgs args)
@@ -217,9 +341,9 @@ namespace FSpot {
 		{
 			gallery_export_dialog.Destroy ();
 
-			if (gallery_add) 
+			if (gallery_add != null) 
 				gallery_add.Dialog.Destroy ();
-			if (album_add)
+			if (album_add != null)
 				album_add.Dialog.Destroy ();
 		}
 
