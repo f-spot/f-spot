cfa83f4094722e2f0e5bfdfd1bc6b8c95c5fab3e
Larry Ewing <lewing@novell.com>
2004-11-30 09:03:12 +0000

    make the error message a ref, newer mono versions seem to be unhappy about
    2004-11-30  Larry Ewing  <lewing@novell.com>
    
    	* src/JpegUtils.cs: make the error message a ref, newer mono
    	versions seem to be unhappy about marshalling an out there.

--- src/JpegUtils.cs
+++ src/JpegUtils.cs
@@ -47,13 +47,13 @@ public class JpegUtils {
 
 	[DllImport ("libfspot")]
 	static extern bool f_transform_jpeg (string source_path, string destination_path, TransformType transform,
-					     out string error_message_return);
+					     ref string error_message_return);
 
 	public static void Transform (string source_path, string destination_path, TransformType transform)
 	{
-		string error_message;
+		string error_message = null;
 
-		if (! f_transform_jpeg (source_path, destination_path, transform, out error_message))
+		if (! f_transform_jpeg (source_path, destination_path, transform, ref error_message))
 			throw new Exception (error_message);
 	}
 }
