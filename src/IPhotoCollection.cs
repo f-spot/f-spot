eb6f123864ff432fa9f7a64bf276d97d7ba2ce8b
Larry Ewing <lewing@src.gnome.org>
2005-01-10 05:41:18 +0000

    missed this

--- src/IPhotoCollection.cs
+++ src/IPhotoCollection.cs
@@ -2,11 +2,10 @@ namespace FSpot {
 	public delegate void IPhotoCollectionChangedHandler (IPhotoCollection collection);
 	public delegate void IPhotoCollectionItemChangedHandler (IPhotoCollection collection, int item);
 
-	public interface IPhotoCollection {
-		Photo [] Photos {
+	public interface IPhotoCollection : IBrowsableCollection {
+	        Photo [] Photos {
 			get;
 		}
-
 #if false
 		event IPhotoCollectionChangedHandler Changed;
 		event IPhotoCollectionItemChangedHandler ItemChanged;
