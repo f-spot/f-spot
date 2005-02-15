8b9f1a09a24121e72dce3786c60d060131fa8028
Larry Ewing <lewing@novell.com>
2005-02-15 02:42:06 +0000

    fix up style and translate strings.
    2005-02-14  Larry Ewing  <lewing@novell.com>
    
    	* src/CameraFileSelectionDialog.cs: fix up style and translate strings.

--- src/CameraFileSelectionDialog.cs
+++ src/CameraFileSelectionDialog.cs
@@ -31,7 +31,7 @@ public class CameraFileSelectionDialog
 	Tag[] selected_tags;
 	bool cancelled;
 	
-	public CameraFileSelectionDialog(GPhotoCamera cam, Db datab)
+	public CameraFileSelectionDialog (GPhotoCamera cam, Db datab)
 	{
 		camera = cam;
 		db = datab;
@@ -39,38 +39,36 @@ public class CameraFileSelectionDialog
 		selected_tags = null;
 	}
 	
-	public Tag[] Tags
-	{
-		get
-		{
+	public Tag[] Tags {
+		get {
 			return selected_tags;
 		}
 	}
 	
 	public bool Run()
 	{	
-		CreateInterface();
+		CreateInterface ();
 		
 		bool allowed_to_exit = false;
 		while (!allowed_to_exit) {
 
 			if (!cancelled) {
-				ResponseType response = (ResponseType) camera_file_selection_dialog.Run();
+				ResponseType response = (ResponseType) camera_file_selection_dialog.Run ();
 			
 				if (response == ResponseType.Ok) {
-					allowed_to_exit = !SaveFiles();
+					allowed_to_exit = !SaveFiles ();
 					
 					if (!cancelled && allowed_to_exit)
-						ImportFiles();
+						ImportFiles ();
 				} else
 					allowed_to_exit = true;
 
 			}
 		}
 		
-		camera_file_selection_dialog.Destroy();
+		camera_file_selection_dialog.Destroy ();
 		
-		return (!cancelled);
+		return !cancelled;
 	}
 	
 	private void CreateInterface ()
@@ -79,59 +77,63 @@ public class CameraFileSelectionDialog
 		gui.Autoconnect (this);
 		
 		file_tree.Selection.Mode = SelectionMode.Multiple;
-		file_tree.AppendColumn("Path", new CellRendererText(), "text", DirectoryColumn);
-		file_tree.AppendColumn("File", new CellRendererText(), "text", FileColumn);
-		file_tree.AppendColumn("Preview", new CellRendererPixbuf(), "pixbuf", PreviewColumn);
-		file_tree.AppendColumn("Index", new CellRendererText(), "text", IndexColumn).Visible = false;
+		file_tree.AppendColumn ("Path", new CellRendererText (), "text", DirectoryColumn);
+		file_tree.AppendColumn ("File", new CellRendererText (), "text", FileColumn);
+		file_tree.AppendColumn ("Preview", new CellRendererPixbuf (), "pixbuf", PreviewColumn);
+		file_tree.AppendColumn ("Index", new CellRendererText (), "text", IndexColumn).Visible = false;
 		
-		preview_list_store = new ListStore(typeof(string), typeof(string), typeof(Pixbuf), typeof(int));
+		preview_list_store = new ListStore (typeof (string), typeof (string), 
+						    typeof (Pixbuf), typeof (int));
+
 		file_tree.Model = preview_list_store;
 		
-		// FIXME: We should get the home directory through gnome, but there doesn't seem to be
-		//a binding for either of the right functions right now, g_get_home_dir() in glib is what
-		//we need here.
-		string home_directory = Environment.GetEnvironmentVariable ("HOME");
-		copied_file_destination.Text = home_directory;
+		copied_file_destination.Text = FSpot.Global.HomeDirectory;
 		
-		GetPreviews();
+		GetPreviews ();
 	}
 	
 	private void GetPreviews()
 	{
-		ProgressDialog pdialog = new ProgressDialog("Downloading Previews", ProgressDialog.CancelButtonType.Cancel, 
-							    camera.FileList.Count, camera_file_selection_dialog);
+		ProgressDialog pdialog = new ProgressDialog (Mono.Posix.Catalog.GetString ("Downloading Previews"), 
+							     ProgressDialog.CancelButtonType.Cancel, 
+							     camera.FileList.Count, 
+							     camera_file_selection_dialog);
 		
 		int index = 0;
 		foreach (GPhotoCameraFile file in camera.FileList) {
-			cancelled = pdialog.Update("Downloading Preview of " + file.FileName);
-			if (cancelled == true) return;
+			string msg = String.Format (Mono.Posix.Catalog.GetString ("Downloading Preview of {0}"), file.FileName);
+			cancelled = pdialog.Update (msg);
+
+			if (cancelled) 
+				return;
 			
-			Pixbuf thumbscale = camera.GetPreviewPixbuf(file);
-			preview_list_store.AppendValues(file.Directory, file.FileName, thumbscale, index);
+			Pixbuf thumbscale = camera.GetPreviewPixbuf (file);
+			preview_list_store.AppendValues (file.Directory, file.FileName, thumbscale, index);
 			index++;
 		}
 		
-		pdialog.Destroy();
+		pdialog.Destroy ();
 	}
 	
-	private bool SaveFiles()
+	private bool SaveFiles ()
 	{
 		TreeSelection selection = file_tree.Selection;
 		TreeModel model;
-		TreePath[] selected_rows = selection.GetSelectedRows(out model);
+		TreePath[] selected_rows = selection.GetSelectedRows (out model);
 	
-		saved_files = new string[selected_rows.Length];
+		saved_files = new string [selected_rows.Length];
 		
-		ProgressDialog pdialog = new ProgressDialog ("Saving Files...", 
+		ProgressDialog pdialog = new ProgressDialog (Mono.Posix.Catalog.GetString ("Saving Files..."), 
 							     ProgressDialog.CancelButtonType.Cancel, 
-							     selected_rows.Length, camera_file_selection_dialog);
+							     selected_rows.Length, 
+							     camera_file_selection_dialog);
 		
 		if (copied_file_destination.Text.Length == 0) {
 			MessageDialog md = new MessageDialog (camera_file_selection_dialog, 
 							      DialogFlags.DestroyWithParent, 
 							      MessageType.Warning, 
 							      ButtonsType.Ok, 
-							      "A destination directory must be chosen.");
+							      Mono.Posix.Catalog.GetString ("A destination directory must be chosen."));
 			md.Run ();
 			md.Destroy ();
 			return true;
@@ -139,46 +141,45 @@ public class CameraFileSelectionDialog
 		
 		int copied_file_count = 0;
 		int file_number_offset = 0;
-		string directory = NormalizeDirectory(copied_file_destination.Text);
-		string prefix = NormalizePrefix(prefix_entry.Text);
+		string directory = NormalizeDirectory (copied_file_destination.Text);
+		string prefix = NormalizePrefix (prefix_entry.Text);
 		string number;
 		string extension;
 		string filename = "";
-		foreach (TreePath cur_row in selected_rows)
-		{
-			if (cancelled == true) return false;
+
+		foreach (TreePath cur_row in selected_rows) {
+			if (cancelled) 
+				return false;
 			
 			TreeIter cur_iter;
-			if (model.GetIter(out cur_iter, cur_row))
-			{
-				int index = (int)model.GetValue(cur_iter, IndexColumn);
-				GPhotoCameraFile cur_cam_file = (GPhotoCameraFile)camera.FileList[index];
+			if (model.GetIter (out cur_iter, cur_row)) {
+				int index = (int) model.GetValue (cur_iter, IndexColumn);
+				GPhotoCameraFile cur_cam_file = (GPhotoCameraFile) camera.FileList [index];
 				
-				extension = System.IO.Path.GetExtension(cur_cam_file.FileName).ToLower();
+				extension = System.IO.Path.GetExtension (cur_cam_file.FileName).ToLower();
 				
 				file_number_offset--;
-				do
-				{
+				do {
 					file_number_offset++;
-					number = PadNumber(copied_file_count + file_number_offset);	
+					number = PadNumber (copied_file_count + file_number_offset);	
 					filename = directory + prefix + number + extension;
-				} while (File.Exists(filename));
+				} while (File.Exists (filename));
 				
-				camera.SaveFile(index, filename);
-				saved_files[copied_file_count] = filename;
+				camera.SaveFile (index, filename);
+				saved_files [copied_file_count] = filename;
 				copied_file_count++;
 			}
 			
-			cancelled = pdialog.Update("Saved File " + filename);
+			string msg = String.Format (Mono.Posix.Catalog.GetString ("Saved File {0}"), filename);
 		}
 		
-		pdialog.Destroy();
+		pdialog.Destroy ();
 		return false;
 	}
 	
 	private void ImportFiles ()
 	{
-		if (saved_files != null && import_files_checkbox.Active == true) {
+		if (saved_files != null && import_files_checkbox.Active) {
 			ImportCommand command = new ImportCommand (null);
 			command.ImportFromPaths (db.Photos, saved_files, selected_tags);
 		}
@@ -186,18 +187,18 @@ public class CameraFileSelectionDialog
 	
 	void HandleSelectSaveDirectory (object sender, EventArgs args)
 	{		
-		FileSelection fs = new FileSelection ("Select save directory...");
+		FileSelection fs = new FileSelection (Mono.Posix.Catalog.GetString ("Select save directory..."));
 		fs.FileList.Sensitive = false;
 		int result = fs.Run ();
 		
-		if ((ResponseType)result == ResponseType.Ok)
-		{
-			copied_file_destination.Text = NormalizeDirectory(fs.Filename);
+		if ((ResponseType)result == ResponseType.Ok) {
 			TreeSelection dirselection = fs.DirList.Selection;
 			TreeModel model;
 			TreeIter iter;
-			if (dirselection.GetSelected(out model, out iter))
-			{
+
+			copied_file_destination.Text = NormalizeDirectory(fs.Filename);
+
+			if (dirselection.GetSelected(out model, out iter)) {
 				string subdirname = (string)model.GetValue(iter, 0);
 				copied_file_destination.Text += subdirname;
 			}
@@ -208,9 +209,9 @@ public class CameraFileSelectionDialog
 	
 	void HandleSelectTags (object sender, EventArgs args)
 	{
-		TagSelectionDialog tag_selection = new TagSelectionDialog(db.Tags);
-		selected_tags = tag_selection.Run();
-		tag_selection.Hide(); 
+		TagSelectionDialog tag_selection = new TagSelectionDialog (db.Tags);
+		selected_tags = tag_selection.Run ();
+		tag_selection.Hide (); 
 	}
 	
 	void HandleImportToggled (object sender, EventArgs args)
@@ -219,30 +220,36 @@ public class CameraFileSelectionDialog
 			select_tag_button.Sensitive = (sender as Gtk.CheckButton).Active;
 	}
 	
-	private string PadNumber(int number)
+	private string PadNumber (int number)
 	{
-		string string_num = number.ToString();
+		string string_num = number.ToString ();
 		
 		return string_num.PadLeft(4, '0');
 	}
 	
-	private bool IsSeparator(char sep)
+	private bool IsSeparator (char sep)
 	{
-		return (sep == System.IO.Path.DirectorySeparatorChar || sep == System.IO.Path.AltDirectorySeparatorChar || sep == System.IO.Path.VolumeSeparatorChar);
+		return (sep == System.IO.Path.DirectorySeparatorChar 
+			|| sep == System.IO.Path.AltDirectorySeparatorChar 
+			|| sep == System.IO.Path.VolumeSeparatorChar);
 	}
 	
-	private string NormalizeDirectory(string dir)
+	private string NormalizeDirectory (string dir)
 	{
-		char lastchar = dir[dir.Length - 1];
-		if (!IsSeparator(lastchar))
+		char lastchar = dir [dir.Length - 1];
+		if (!IsSeparator (lastchar))
 			return dir + System.IO.Path.DirectorySeparatorChar;
 		return dir;
 	}
 	
-	private string NormalizePrefix(string pre)
+	private string NormalizePrefix (string pre)
 	{
-		if (pre.Length == 0) return pre;
-		if (pre[pre.Length - 1] != ' ') return pre + ' ';
+		if (pre.Length == 0) 
+			return pre;
+
+		if (pre [pre.Length - 1] != ' ') 
+			return pre + ' ';
+
 		return pre;
 	}
 }
