caa68c56a77bf0cc49881c04e36f78854f69132b
Larry Ewing <lewing@src.gnome.org>
2005-02-17 06:34:14 +0000

    missed this

---
+++ src/GladeDialog.cs
@@ -0,0 +1,33 @@
+namespace FSpot {
+	public class GladeDialog {
+		protected string dialog_name;
+		protected Glade.XML xml;
+		private Gtk.Dialog dialog;
+		
+		protected GladeDialog ()
+		{
+
+		}
+
+		public GladeDialog (string name)
+		{
+			CreateDialog (name);
+		}
+
+		protected void CreateDialog (string name)
+		{
+			this.dialog_name = name;		
+			xml = new Glade.XML (null, "f-spot.glade", name, null);
+			xml.Autoconnect (this);
+		}
+
+		public Gtk.Dialog Dialog {
+			get {
+				if (dialog == null)
+					dialog = (Gtk.Dialog) xml.GetWidget (dialog_name);
+				
+				return dialog;
+			}
+		}
+	}
+}
