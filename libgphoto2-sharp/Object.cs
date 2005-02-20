87fe171a6de60d6c756cda955454458239f80fb6
Larry Ewing <lewing@novell.com>
2005-02-20 21:40:32 +0000

    renable the Cleanup bits of the dispose call.
    2005-02-20  Larry Ewing  <lewing@novell.com>
    
    	* libgphoto2-sharp/Object.cs: renable the Cleanup bits of the dispose call.

---
+++ libgphoto2-sharp/Object.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace LibGPhoto2 {
+	public abstract class Object : System.IDisposable {
+		protected HandleRef handle;
+		
+		public HandleRef Handle {
+			get {
+				return handle;
+			}
+		}
+		
+		public Object () {}
+
+		public Object (IntPtr ptr)
+		{
+			handle = new HandleRef (this, ptr);
+		}
+		
+		protected abstract void Cleanup ();
+		
+		public void Dispose () {
+			Cleanup ();
+			System.GC.SuppressFinalize (this);
+		}
+		
+		~Object ()
+		{
+			System.Console.WriteLine ("Finalized {0}", this.ToString ());
+			//Cleanup ();
+		}
+	}
+}
