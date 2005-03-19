cea1bd4f1573d8e8b18e8aff8dd794bb25d9b35f
Larry Ewing <lewing@novell.com>
2005-03-19 12:30:41 +0000

    add exit button to fullscreen mode.
    2005-03-19  Larry Ewing  <lewing@novell.com>
    
    	* src/FullScreenView.cs: add exit button to fullscreen mode.

--- src/FullScreenView.cs
+++ src/FullScreenView.cs
@@ -1,34 +1,126 @@
 namespace FSpot {
+	public class ScrolledView : Gtk.Fixed {
+		private Gtk.EventBox ebox;
+		private Gtk.ScrolledWindow scroll;
+		private Delay hide;
+
+		public ScrolledView () : base () {
+			scroll = new Gtk.ScrolledWindow  (null, null);
+			this.Put (scroll, 0, 0);
+			scroll.Show ();
+			
+			ebox = new Gtk.EventBox ();
+			this.Put (ebox, 0, 0);
+			ebox.ShowAll ();
+			
+			hide = new Delay (2000, new GLib.IdleHandler (HideControls));
+		}
+
+		public bool HideControls ()
+		{
+			hide.Stop ();
+			ebox.Hide ();
+			return false;
+		}
+		
+		public void ShowControls ()
+		{
+			hide.Stop ();
+			hide.Start ();
+			ebox.Show ();
+		}
+
+		protected override void OnDestroyed ()
+		{
+			base.OnDestroyed ();
+			hide.Stop ();
+		}
+
+		public Gtk.EventBox ControlBox {
+			get {
+				return ebox;
+			}
+		}
+		public Gtk.ScrolledWindow ScrolledWindow {
+			get {
+				return scroll;
+			}
+		}
+
+		protected override void OnSizeAllocated (Gdk.Rectangle allocation)
+		{
+			scroll.SetSizeRequest (allocation.Width, allocation.Height);
+			base.OnSizeAllocated (allocation);
+		}
+	}
+
 	public class FullScreenView : Gtk.Window {
+		private ScrolledView scroll;
+		private PhotoImageView view;
+		private Gtk.Button forward_button;
+		private Gtk.Button back_button;
+		
 		public FullScreenView (PhotoQuery query) : base ("Full Screen Mode")
 		{
 			try {
-				scroll = new Gtk.ScrolledWindow (null, null);
+				//scroll = new Gtk.ScrolledWindow (null, null);
+				
+				scroll = new ScrolledView ();
 				view = new PhotoImageView (query);
 				view.ModifyBg (Gtk.StateType.Normal, this.Style.Black);
 				view.PointerMode = ImageView.PointerModeType.Scroll;
 				this.Add (scroll);
-				scroll.Add (view);
-				scroll.ShowAll ();
+				view.Show ();
+				view.MotionNotifyEvent += HandleViewMotion;
+
+				scroll.ScrolledWindow.Add (view);
+
+				Gtk.Button close = ExitButton ();
+				scroll.ControlBox.Add (close);
+				close.Clicked += HandleExitClicked;
+				close.Show ();
+				scroll.ShowControls ();
+				
+				scroll.Show ();
 				this.Decorated = false;
 				this.Fullscreen ();
 				this.ButtonPressEvent += HandleButtonPressEvent;
+				
+				view.GrabFocus ();
 			} catch (System.Exception e) {
 				System.Console.WriteLine (e);
 			}		      
 		}
 
-		private Gtk.ScrolledWindow scroll;
-		private PhotoImageView view;
+		private Gtk.Button ExitButton ()
+		{
+			Gtk.HBox hbox = new Gtk.HBox ();
+			hbox.PackStart (new Gtk.Image (Gtk.Stock.Quit, Gtk.IconSize.Button));
+			hbox.PackStart (new Gtk.Label (Mono.Posix.Catalog.GetString ("Exit fullscreen")));
+			hbox.ShowAll ();
+			return new Gtk.Button (hbox);
+		}
+
+		private void HandleExitClicked (object sender, System.EventArgs args)
+		{
+			this.Destroy ();
+		}
+
+		[GLib.ConnectBefore]
+		private void HandleViewMotion (object sender, Gtk.MotionNotifyEventArgs args)
+		{
+			int x, y;
+			Gdk.ModifierType type;
+			view.GdkWindow.GetPointer (out x, out y, out type);
+			scroll.ShowControls ();
+		}
+
 		public PhotoImageView View {
 			get {
 				return view;
 			}
 		}
-		
-		private Gtk.Button forward_button;
-		private Gtk.Button back_button;
-		
+
 		private void HandleButtonPressEvent (object sender, Gtk.ButtonPressEventArgs args)
 		{
 			if (args.Event.Type == Gdk.EventType.ButtonPress
@@ -38,6 +130,7 @@ namespace FSpot {
 			}
 		}
 
+		
 		protected override bool OnKeyPressEvent (Gdk.EventKey key)
 		{
 			if (key == null) {
