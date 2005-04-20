d28c91f01e9c75900b321e5342d3508da296409b
Larry Ewing <lewing@novell.com>
2005-04-20 22:13:52 +0000

    add rgb565 writing with accompanying command line arguments.
    2005-04-20  Larry Ewing  <lewing@novell.com>
    
    	* src/IthmbDb.cs: add rgb565 writing with accompanying command
    	line arguments.

--- src/IthmbDb.cs
+++ src/IthmbDb.cs
@@ -14,32 +14,27 @@ public class IthmbDb {
 		this.Height = height;
 		this.YUV = yuv;
 	}
-
+	
         public static IthmbDb Thumbnail = new IthmbDb ("F1009", 42, 30, false);
 	public static IthmbDb Slide = new IthmbDb ("F1015", 130, 88, false);
 	public static IthmbDb FullScreen = new IthmbDb ("F1013", 176, 220, false);
 	public static IthmbDb External = new IthmbDb ("F1019", 720, 480, true);
-
+	
 	public void LoadRgb565 (BinaryReader reader, Gdk.Pixbuf dest)
 	{
 		unsafe {
 			byte * pixels;
-			ushort s;
 			int row, col;
+			ushort s;
 
 			for (row = 0; row < dest.Height; row++) {
 				pixels = ((byte *)dest.Pixels) + row * dest.Rowstride;
 				for (col = 0; col < dest.Width; col++) {
 					s = reader.ReadUInt16 ();
-#if true
+
 					*(pixels++) = (byte)(((s >> 8) & 0xf8) | ((s >> 13) & 0x7)); // r
 					*(pixels++) = (byte)(((s >> 3) & 0xfc) | ((s >> 9) & 0x3));  // g
 					*(pixels++) = (byte)(((s << 3) & 0xf8) | ((s >> 2) & 0x7));  // b
-#else
-					*(pixels++) = (byte)(((s & 0x7c00) >> 7) | ((s & 0x7000) >> 12)); // r
-					*(pixels++) = (byte)(((s & 0x03e0) >> 2) | ((s & 0x0380) >> 7));  // g
-					*(pixels++) = (byte)(((s & 0x001f) << 3) | ((s & 0x001c) >> 2));  // b
-#endif
 				}
 			}
 		}
@@ -57,14 +52,35 @@ public class IthmbDb {
 					*(pixels++) = reader.ReadByte ();
 					*(pixels++) = reader.ReadByte ();
 					*(pixels++) = reader.ReadByte ();
-				 }
-			 }
-		 }
-	 }
+				}
+			}
+		}
+	}
+	
+	public void LoadRgb555 (BinaryReader reader, Gdk.Pixbuf dest)
+	{
+		unsafe {
+			byte * pixels;
+			int row, col;
+			ushort s;
+			
+			for (row = 0; row < dest.Height; row++) {
+				pixels = ((byte *)dest.Pixels) + row * dest.Rowstride;
+				for (col = 0; col < dest.Width; col++) {
+					s = reader.ReadUInt16 ();
 
-	 public void LoadIYUV (BinaryReader reader, Gdk.Pixbuf dest)
-	 {
-		 unsafe {
+					/* rgb555 */
+					*(pixels++) = (byte)(((s & 0x7c00) >> 7) | ((s & 0x7000) >> 12)); // r
+					*(pixels++) = (byte)(((s & 0x03e0) >> 2) | ((s & 0x0380) >> 7));  // g
+					*(pixels++) = (byte)(((s & 0x001f) << 3) | ((s & 0x001c) >> 2));  // b
+				}
+			}
+		}
+	}
+
+	public void LoadIYUV (BinaryReader reader, Gdk.Pixbuf dest)
+	{
+		unsafe {
 			 byte * pixels;
 			 ushort y0, y1, u, v;
 			 int row, col;
@@ -142,7 +158,7 @@ public class IthmbDb {
 		 FileStream stream = new FileStream (path, FileMode.Open);
 		 
 		 
-		 stream.Position = width * height * 4 * offset;
+		 stream.Position = width * height * 2 * offset;
 		 BinaryReader reader = new BinaryReader (stream);
 		 
 		 if (!this.YUV) {
@@ -151,7 +167,50 @@ public class IthmbDb {
 			 LoadIYUV (reader, image);
 		 }
 		 
+		 stream.Close ();
 		 return image;
+	}
+
+	public void Save (Gdk.Pixbuf src, string path, int offset) 
+	{
+		 int width = this.Width;
+		 int height = this.Height;
+		 int size;
+		 int row, col;
+		 byte r, g, b;
+		 ushort [] packed;
+		 int i;
+
+		 path = Path.Combine (path, this.Name + "_1.ithmb");
+		 packed = new ushort [width * height];
+		 i = 0;
+		 unsafe {
+			 byte * pixels;
+
+			 for (row = 0; row < height; row ++) {
+				 pixels = ((byte *)src.Pixels) + row * src.Rowstride;
+				 
+				 for (col = 0; col < width; col ++) {
+					 r = *(pixels ++);
+					 g = *(pixels ++);
+					 b = *(pixels ++);
+
+					 if (!this.YUV) 					 
+						 packed [i ++] = (ushort) (((r & 0xf8) << 8) | ((g & 0xfc) << 3) | (b >> 3));
+					 else {
+						 throw new System.Exception ("this isn't supported yet");
+					 }
+				 }
+			 }
+		 }
+		 
+		 FileStream stream = new FileStream (path, FileMode.Open);
+		 stream.Position = width * height * 2 * offset;
+		 BinaryWriter writer = new BinaryWriter (stream);
+		 foreach (short val in packed) {
+			 writer.Write (val);
+		 }
+		 stream.Close ();
 	}
 
 	static void Main (string [] args) 
@@ -162,20 +221,26 @@ public class IthmbDb {
 		Gtk.VBox vbox = new Gtk.VBox ();
 		win.Add (hbox);
 		hbox.PackStart (vbox);
-		
-		Gdk.Pixbuf thumb = IthmbDb.Thumbnail.Load (args [0], System.Int32.Parse (args [1]));
+
+		string basepath = args [0];
+		int index = System.Int32.Parse (args [1]);
+
+		Gdk.Pixbuf thumb = IthmbDb.Thumbnail.Load (basepath, index);
 		Gtk.Image image = new Gtk.Image (thumb);
 		vbox.PackStart (image);
 		
-		thumb = IthmbDb.Slide.Load (args [0], System.Int32.Parse (args [1]));
+		if (args.Length > 2)
+			IthmbDb.Thumbnail.Save (thumb, basepath, System.Int32.Parse (args [2]));
+		
+		thumb = IthmbDb.Slide.Load (basepath, index);
 		image = new Gtk.Image (thumb);
 		vbox.PackStart (image);
 		
-		thumb = IthmbDb.FullScreen.Load (args [0], System.Int32.Parse (args [1]));
+		thumb = IthmbDb.FullScreen.Load (basepath, index);
 		image = new Gtk.Image (thumb);
 		vbox.PackStart (image);
 
-		thumb = IthmbDb.External.Load (args [0], System.Int32.Parse (args [1]));
+		thumb = IthmbDb.External.Load (basepath, index);
 		image = new Gtk.Image (thumb);
 		hbox.PackStart (image);
 
