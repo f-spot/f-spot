5a3988c1c284970823b4de84dfbc43927285d25b
Larry Ewing <lewing@novell.com>
2005-08-15 10:25:06 +0000

    Load the first image in the PhotoView as soon as there is one.
    2005-08-15  Larry Ewing  <lewing@novell.com>
    
    	* src/ImportCommand.cs: Load the first image in the PhotoView as
    	soon as there is one.

--- src/ImportCommand.cs
+++ src/ImportCommand.cs
@@ -410,10 +410,19 @@ public class ImportCommand : FSpot.GladeDialog {
 		progress_bar.Fraction = (double) count / System.Math.Max (total, 1);
 	}
 
-	private void HandleTraySelectionChanged (FSpot.IBrowsableCollection collection) 
+	private void HandleCollectionChanged (FSpot.IBrowsableCollection col)
 	{
-		if (collection.Count > 0)
+		if (collection.Count == 1) {
+			photo_view.Item.Index = -1;
+			photo_view.Item.Index = 0;
+		}
+	}
+
+	private void HandleTraySelectionChanged (FSpot.IBrowsableCollection coll) 
+	{
+		if (tray.Selection.Count > 0)
 			photo_view.Item.Index = tray.Selection.Ids[0];
+		
 	}
 
 	private bool Step ()
@@ -633,6 +642,7 @@ public class ImportCommand : FSpot.GladeDialog {
 		source_option_menu.Menu = menu;
 
 		collection = new FSpot.PhotoList (new Photo [0]);
+		collection.Changed += HandleCollectionChanged;
 		tray = new TrayView (collection);
 		tray.Selection.Changed += HandleTraySelectionChanged;
 		icon_scrolled.SetSizeRequest (200, 480);
