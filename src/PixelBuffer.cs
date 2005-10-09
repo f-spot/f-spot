1a7d34145fda6bf9eccf6a74aa14efa4b5402f0b
Larry Ewing <lewing@novell.com>
2005-10-09 16:34:08 +0000

    fix the loading interface.
    2005-10-09  Larry Ewing  <lewing@novell.com>
    
    	* src/PixelBuffer.cs: fix the loading interface.

--- src/PixelBuffer.cs
+++ src/PixelBuffer.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace FSpot.Imaging {
 	public enum PixelBufferDepth {
 		UInt8 = 8,
@@ -11,39 +13,106 @@ namespace FSpot.Imaging {
 		{
 			this.width = width;
 			this.height = height;
+			this.nchannels = 3;
+
 			depth = PixelBufferDepth.UInt16;
 
-			row_stride = width;
+			row_stride = width * 3;
 
-			data = new ushort [width * height];
+			data = new ushort [width * height * nchannels];
 		}
 
-		public UInt16Buffer (Gdk.Pixbuf pixbuf, Cms.Profile profile)
+		public override void Fill8 (int i, int j, byte [] src_data, int offset, int count)
 		{
-			for (int r = 0; r < height; r++) {
-				
 
+		}
+
+		public override void Fill16 (int i, int j, byte [] src_data, int offset, int count, bool little)
+		{
+			if (System.BitConverter.IsLittleEndian == little)
+				Fill16 (i, j, src_data, offset, count);
+			else
+				Fill16Swap (i, j, src_data, offset, count);
+		}
+
+		public void Fill16 (int i, int j, byte [] src_data, int offset, int count)
+		{ 
+			unsafe {
+				ushort * rowpix;
+
+				fixed (ushort * pixels = &data [0]) {
+					fixed (ushort * src_pixels = &src_data [offset]) {
+						rowpix = pixels + i * row_stride + j;
+
+						for (int col = 0; col < count; col ++)
+							rowpix [col] = src_pixels [col];
+					}
+				}
 			}
 		}
 
-		public ToPixbuf (Cms.Profile destination_profile)
+		public void Fill16Swap (int i, int j, byte [] src_data, int offset, int count)
 		{
-			Cms.Profile [] list = new Cms.Profile { profile, destination_profile };
+			unsafe {
+				ushort * rowpix;
+
+				fixed (ushort * pixels = &data [0]) {
+					fixed (ushort * src_pixels = &src_data [offset]) {
+						rowpix = pixels + i * row_stride + j;
+
+						for (int col = 0; col < count; col ++)
+							rowpix [col] = BitConverter.Swap (src_pixels [col]);
+					}
+				}
+			}
+		}
+		
+		public override Gdk.Pixbuf ToPixbuf (Cms.Profile destination_profile)
+		{
+#if true //USE_LCMS
+			profile = Cms.Profile.CreateSRgb ();
+			Cms.Profile [] list = new Cms.Profile [] { profile, destination_profile };
 			Gdk.Pixbuf pixbuf = new Gdk.Pixbuf (Gdk.Colorspace.Rgb, false, 8, 
-							    orig.Width, orig.Height);
+							    width, height);
+			
 			Cms.Transform t = new Cms.Transform (list,
-							     
+							     Cms.Format.Rgb16,
+							     PixbufUtils.PixbufCmsFormat (pixbuf),
+							     Cms.Intent.Perceptual, 0x0);
 			
-		}
+			unsafe {
+				fixed (ushort * srcpix  = &data[0]) {
+					byte * destpix = (byte *) pixbuf.Pixels;
+					
+					for (int row = 0; row < height; row++)
+						t.Apply ((IntPtr) (srcpix + row * row_stride),
+							 (IntPtr) (destpix + row * pixbuf.Rowstride),
+							 (uint)width);
+				}
+			}
 
-		public override void Fill (int x, int y, ushort [] data, int offset, int length)
-		{
-			
-		}
+			return pixbuf;
+#else
+			Gdk.Pixbuf pixbuf = new Gdk.Pixbuf (Gdk.Colorspace.Rgb, false, 8, 
+							    width, height);
 
-		public override void Fill (int x, int y, byte [] data, int offset, int length)
-		{
-			
+			unsafe {
+				fixed (ushort * src  = &data[0]) {
+					ushort * srcpix = src;
+					byte * destpix = (byte *) pixbuf.Pixels;
+					
+					for (int row = 0; row < height; row++) {
+						for (int col = 0; col < width * nchannels; col++)
+							destpix [col] = (byte) (srcpix [col] >> 8);
+						
+						srcpix += row_stride;
+						destpix += pixbuf.Rowstride;
+					}
+				}
+			}
+
+			return pixbuf;
+#endif
 		}
 	}
 
@@ -68,33 +137,31 @@ namespace FSpot.Imaging {
 		protected int row_stride;
 		protected int width;
 		protected int height;
+		protected int nchannels;
 		protected PixelBufferDepth depth;
 		protected Cms.Profile profile;
 
-		new PixelBuffer (int width, int height, PixelBufferDepth depth)
+		protected PixelBuffer ()
 		{
-			if 
 
 		}
 
-		void Fill (int row, int col, ushort [] value)
-		{
-			Fill (row, col, value, 0, value.Length);
+		public int Width {
+			get {
+				return width;
+			}
 		}
 
-		void abstract Fill (int x, int y, ushort [] value, int start_offset, int len)
-		{
-			
+		public int Height {
+			get {
+				return height;
+			}
 		}
+					   
+		public abstract void Fill8 (int row, int col, byte [] value, int start_offset, int len);
 
-		void Fill (int row, int col, byte [] value)
-		{
-			Fill (row, col, value, 0, value.Length);
-		}
+		public abstract void Fill16 (int row, int col, byte [] data, int offset, int len, bool little);
 
-		void abstract Fill (int row, int col, byte [] value, int start_offset, int len)
-		{
-			
-		}
+		public abstract Gdk.Pixbuf ToPixbuf (Cms.Profile profile);
 	}
 }
