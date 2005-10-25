a9682f5df6dd4a040dbfcd766b6eda580a446497
Larry Ewing <lewing@novell.com>
2005-10-25 17:00:13 +0000

    remove the size specifier.
    2005-10-25  Larry Ewing  <lewing@novell.com>
    
    	* src/Cms.cs: remove the size specifier.

--- src/Cms.cs
+++ src/Cms.cs
@@ -21,30 +21,30 @@ namespace Cms {
 
 	public enum IccColorSpace : uint {
 		XYZ                        = 0x58595A20,  /* 'XYZ ' */
-		Lab                        = 0x4C616220L,  /* 'Lab ' */
-		Luv                        = 0x4C757620L,  /* 'Luv ' */
-		YCbCr                      = 0x59436272L,  /* 'YCbr' */
-		Yxy                        = 0x59787920L,  /* 'Yxy ' */
-		Rgb                        = 0x52474220L,  /* 'RGB ' */
-		Gray                       = 0x47524159L,  /* 'GRAY' */
-		Hsv                        = 0x48535620L,  /* 'HSV ' */
-		Hls                        = 0x484C5320L,  /* 'HLS ' */
-		Cmyk                       = 0x434D594BL,  /* 'CMYK' */
-		Cmy                        = 0x434D5920L,  /* 'CMY ' */
-		Color2                     = 0x32434C52L,  /* '2CLR' */
-		Color3                     = 0x33434C52L,  /* '3CLR' */
-		Color4                     = 0x34434C52L,  /* '4CLR' */
-		Color5                     = 0x35434C52L,  /* '5CLR' */
-		Color6                     = 0x36434C52L,  /* '6CLR' */
-		Color7                     = 0x37434C52L,  /* '7CLR' */
-		Color8                     = 0x38434C52L,  /* '8CLR' */
-		Color9                     = 0x39434C52L,  /* '9CLR' */
-		Color10                    = 0x41434C52L,  /* 'ACLR' */
-		Color11                    = 0x42434C52L,  /* 'BCLR' */
-		Color12                    = 0x43434C52L,  /* 'CCLR' */
-		Color13                    = 0x44434C52L,  /* 'DCLR' */
-		Color14                    = 0x45434C52L,  /* 'ECLR' */
-		Color15                    = 0x46434C52L,  /* 'FCLR' */
+		Lab                        = 0x4C616220,  /* 'Lab ' */
+		Luv                        = 0x4C757620,  /* 'Luv ' */
+		YCbCr                      = 0x59436272,  /* 'YCbr' */
+		Yxy                        = 0x59787920,  /* 'Yxy ' */
+		Rgb                        = 0x52474220,  /* 'RGB ' */
+		Gray                       = 0x47524159,  /* 'GRAY' */
+		Hsv                        = 0x48535620,  /* 'HSV ' */
+		Hls                        = 0x484C5320,  /* 'HLS ' */
+		Cmyk                       = 0x434D594B,  /* 'CMYK' */
+		Cmy                        = 0x434D5920,  /* 'CMY ' */
+		Color2                     = 0x32434C52,  /* '2CLR' */
+		Color3                     = 0x33434C52,  /* '3CLR' */
+		Color4                     = 0x34434C52,  /* '4CLR' */
+		Color5                     = 0x35434C52,  /* '5CLR' */
+		Color6                     = 0x36434C52,  /* '6CLR' */
+		Color7                     = 0x37434C52,  /* '7CLR' */
+		Color8                     = 0x38434C52,  /* '8CLR' */
+		Color9                     = 0x39434C52,  /* '9CLR' */
+		Color10                    = 0x41434C52,  /* 'ACLR' */
+		Color11                    = 0x42434C52,  /* 'BCLR' */
+		Color12                    = 0x43434C52,  /* 'CCLR' */
+		Color13                    = 0x44434C52,  /* 'DCLR' */
+		Color14                    = 0x45434C52,  /* 'ECLR' */
+		Color15                    = 0x46434C52,  /* 'FCLR' */
 	}
 
 	public enum Intent {
