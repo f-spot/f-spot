5507f94d26e6330f4ac4a14abe92604686483c92
Larry Ewing <lewing@novell.com>
2005-12-15 18:41:21 +0000

    wrap the output stream with a wrapper to keep it from being disposed
    2005-12-15  Larry Ewing  <lewing@novell.com>
    
    	* src/DCRawFile.cs: wrap the output stream with a wrapper to keep
    	it from being disposed prematurely.  Add a new method to provide a
    	stream.

--- src/DCRawFile.cs
+++ src/DCRawFile.cs
@@ -1,18 +1,93 @@
+using System.Diagnostics;
+using System.IO;
+using System;
+
 namespace FSpot {
+	public class Pipe : System.IO.Stream {
+		// This class is a hack to make sure mono doesn't dispose the process
+		// and by extension the stream from the pipe when we are still using the
+		// the stream.
+		Process process;
+		Stream stream;
+
+		public override bool CanRead {
+			get { return stream.CanRead; }
+		}
+
+		public override bool CanSeek {
+			get { return stream.CanSeek; }
+		}
+
+		public override bool CanWrite {
+			get { return stream.CanWrite; }
+		}
+		
+		public override long Length {
+			get { return stream.Length; }
+		}
+
+		public override long Position {
+			get { return stream.Position; }
+			set { stream.Position = value; }
+		}
+
+		public Pipe (Process p, Stream stream)
+		{
+			this.process = p;
+			this.stream = stream;
+		}
+		
+		public override void Flush ()
+		{
+			stream.Flush ();
+		}
+
+		public override int Read (byte [] b, int s, int l)
+		{
+			return stream.Read (b, s, l);
+		}
+
+		public override long Seek (long l, SeekOrigin origin)
+		{
+			return stream.Seek(l, origin);
+		}
+		
+		public override void SetLength (long l)
+		{
+			stream.SetLength (l);
+		}
+
+		public override void Write (byte [] b, int s, int l)
+		{
+			stream.Write (b, s, l);
+		}
+
+		public void Dispose ()
+		{
+			stream = null;
+			process = null;
+		}
+	}
+
 	public class DCRawFile : ImageFile {
 		public DCRawFile (string path) : base (path) {}
 			
 		public override System.IO.Stream PixbufStream ()
 		{
+			return new RawPixbufStream (this.path);
+		}
+
+		public static System.IO.Stream RawPixbufStream (string path)
+		{
 			// FIXME this filename quoting is super lame
-			string args = System.String.Format ("-h -w -c \"{0}\"", this.path);
+			string args = System.String.Format ("-h -w -c \"{0}\"", path);
 
 			System.Diagnostics.Process process = new System.Diagnostics.Process ();
 			process.StartInfo = new System.Diagnostics.ProcessStartInfo ("dcraw", args);
 			process.StartInfo.RedirectStandardOutput = true;
 			process.StartInfo.UseShellExecute = false;
 			process.Start ();
-			return new System.IO.BufferedStream (process.StandardOutput.BaseStream);
+			return new Pipe (process, process.StandardOutput.BaseStream);
 		}
 		
 		public static Gdk.Pixbuf Load (string path, string args)
@@ -27,7 +102,7 @@ namespace FSpot {
 				process.StartInfo.RedirectStandardOutput = true;
 				process.StartInfo.UseShellExecute = false;
 				process.Start ();
-				return PixbufUtils.LoadFromStream (new System.IO.BufferedStream (process.StandardOutput.BaseStream));
+				return PixbufUtils.LoadFromStream (process.StandardOutput.BaseStream);
 			}
 		}
 	}
