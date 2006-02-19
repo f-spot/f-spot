03d1930b1c28861a0f0cae288fd93be082342c79
Larry Ewing <lewing@novell.com>
2006-02-19 23:17:33 +0000

    make slideview work when run standalone.
    2006-02-19  Larry Ewing  <lewing@novell.com>
    
    	* src/SlideView.cs: make slideview work when run standalone.

--- src/SlideView.cs
+++ src/SlideView.cs
@@ -8,39 +8,49 @@ using System.Runtime.InteropServices;
 
 namespace FSpot {
 	public class XScreenSaverSlide : Gtk.Window {
+		public const string ScreenSaverEnviroment = "XSCREENSAVER_WINDOW";
+
 		public XScreenSaverSlide () : base ("")
 		{
 		}
 	       
 		protected override void OnRealized ()
 		{
-			string env = Environment.GetEnvironmentVariable ("XSCREENSAVER_WINDOW");
+			string env = Environment.GetEnvironmentVariable (ScreenSaverEnviroment);
+			
+			if (env != null) {
+				try {
+					env = env.ToLower ();
+					
+					if (env.StartsWith ("0x"))
+						env = env.Substring (2);
 
-			env = env.ToLower ();
-			try {
-				if (env.StartsWith ("0x"))
-					env = env.Substring (2);
-
-				uint xid = UInt32.Parse (env, System.Globalization.NumberStyles.HexNumber);
-
-				GdkWindow = Gdk.Window.ForeignNew (xid);
-				GdkWindow.Events = EventMask.ExposureMask | EventMask.StructureMask | EventMask.EnterNotifyMask 
-					| EventMask.LeaveNotifyMask | EventMask.FocusChangeMask;
-				
-				Style.SetBackground (GdkWindow, Gtk.StateType.Normal);
-				GdkWindow.SetDecorations ((Gdk.WMDecoration) 0);
-				GdkWindow.UserData = this.Handle;
-				SetFlag (WidgetFlags.Realized);
-				SizeRequest ();
-				Gdk.Rectangle geom;
-				int depth;
-				GdkWindow.GetGeometry (out geom.X, out geom.Y, out geom.Width, out geom.Height, out depth);
-				SizeAllocate (new Gdk.Rectangle (geom.X, geom.Y, geom.Width, geom.Height));
-				Resize (geom.Width, geom.Height);
-			} catch (System.Exception e) {
-				System.Console.WriteLine (e);
-				base.OnRealized ();
+					uint xid = UInt32.Parse (env, System.Globalization.NumberStyles.HexNumber);
+					
+					GdkWindow = Gdk.Window.ForeignNew (xid);
+					GdkWindow.Events = EventMask.ExposureMask | EventMask.StructureMask | EventMask.EnterNotifyMask 
+						| EventMask.LeaveNotifyMask | EventMask.FocusChangeMask;
+					
+					Style.SetBackground (GdkWindow, Gtk.StateType.Normal);
+					GdkWindow.SetDecorations ((Gdk.WMDecoration) 0);
+					GdkWindow.UserData = this.Handle;
+					SetFlag (WidgetFlags.Realized);
+					SizeRequest ();
+					Gdk.Rectangle geom;
+					int depth;
+					GdkWindow.GetGeometry (out geom.X, out geom.Y, out geom.Width, out geom.Height, out depth);
+					SizeAllocate (new Gdk.Rectangle (geom.X, geom.Y, geom.Width, geom.Height));
+					Resize (geom.Width, geom.Height);
+					return;
+				} catch (System.Exception e) {
+					System.Console.WriteLine (e);
+				}
+			} else {
+				System.Console.WriteLine ("{0} not set, falling back to window");
 			}
+
+			SetSizeRequest (640, 480);
+			base.OnRealized ();
 		}
 	}
 
