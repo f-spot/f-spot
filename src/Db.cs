105515753b10da262beeee27b3d137704eec342c
Larry Ewing <lewing@novell.com>
2006-06-09 21:08:16 +0000

    fix the time round trip calculation but still revert to the old time
    2006-06-09  Larry Ewing  <lewing@novell.com>
    
    	* src/Db.cs: fix the time round trip calculation but still revert
    	to the old time functions until an updater is ready.

--- src/Db.cs
+++ src/Db.cs
@@ -353,6 +353,7 @@ public class Db : IDisposable {
 
 
 public class DbUtils {
+#if USE_CORRECT_FUNCTION
 	public static DateTime DateTimeFromUnixTime (long unix_time)
 	{
 		DateTime date_time = new DateTime (1970, 1, 1);
@@ -361,6 +362,18 @@ public class DbUtils {
 
 	public static long UnixTimeFromDateTime (DateTime date_time)
 	{
+		return (long) (date_time.ToUniversalTime () - new DateTime (1970, 1, 1)).TotalSeconds;
+	}
+#else
+	public static DateTime DateTimeFromUnixTime (long unix_time)
+	{
+		DateTime date_time = new DateTime (1970, 1, 1).ToLocalTime ();
+		return date_time.AddSeconds (unix_time);
+	}
+	
+	public static long UnixTimeFromDateTime (DateTime date_time)
+	{
 		return (long) (date_time - new DateTime (1970, 1, 1).ToLocalTime ()).TotalSeconds;
 	}
+#endif
 }
