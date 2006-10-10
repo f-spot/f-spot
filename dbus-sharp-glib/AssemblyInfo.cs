769f015bd5907366de09cd544cda849f5c8b24a8
Larry Ewing <lewing@src.gnome.org>
2006-10-10 19:20:13 +0000

    Add dbus-sharp-glib to repo

---
+++ dbus-sharp-glib/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+//[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyTitle ("NDesk.DBus.GLib")]
+[assembly: AssemblyDescription ("GLib integration for NDesk.DBus, the D-Bus IPC library")]
+[assembly: AssemblyCopyright ("Copyright (C) Alp Toker")]
+[assembly: AssemblyCompany ("NDesk")]
