3cb4de0368b890dddf75aa59d99cf4cefb1f945f
Larry Ewing <lewing@novell.com>
2006-11-16 16:39:18 +0000

    add unicode to the description test.
    2006-11-16  Larry Ewing  <lewing@novell.com>
    
    	* src/JpegFile.cs: add unicode to the description test.

--- src/JpegFile.cs
+++ src/JpegFile.cs
@@ -343,22 +343,27 @@ namespace FSpot {
 			Assert.AreEqual (PixbufOrientation.TopLeft, jimg.Orientation);
 		}
 #endif
-
 		[Test]
 		public void TestSave ()
 		{
 			string desc = "this is an example description";
+			string desc2 = "\x00a9 Novell Inc.";
+			PixbufOrientation orient = PixbufOrientation.TopRight;
 			Gdk.Pixbuf test = new Gdk.Pixbuf (System.Reflection.Assembly.GetEntryAssembly (), "f-spot-32.png");
 			string path = ImageFile.TempPath ("joe.jpg");
 			
 			PixbufUtils.SaveJpeg (test, path, 75, new Exif.ExifData ());
 			JpegFile jimg = new JpegFile (path);
 			jimg.SetDescription (desc);
-			jimg.SetOrientation (PixbufOrientation.TopRight);
+			jimg.SetOrientation (orient);
 			jimg.SaveMetaData (path);
 			JpegFile mod = new JpegFile (path);
-			Assert.AreEqual (mod.Orientation, PixbufOrientation.TopRight);
+			Assert.AreEqual (mod.Orientation, orient);
 			Assert.AreEqual (mod.Description, desc);
+			jimg.SetDescription (desc2);
+			jimg.SaveMetaData (path);
+			mod = new JpegFile (path);
+			Assert.AreEqual (mod.Description, desc2);
 		}
 	}
 }
