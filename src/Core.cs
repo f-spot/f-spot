f2726619afd9b4749a5e4857964b3a00bae8c9ea
Stephane Delcroix <sdelcroix@src.gnome.org>
2006-11-25 20:40:08 +0000

    Fixes for bgo 379188.

--- src/Core.cs
+++ src/Core.cs
@@ -57,8 +57,8 @@ namespace FSpot {
 
 		public void RegisterServer ()
 		{
-#if DEBUG_DBUS
 			NameReply nameReply = Bus.Session.RequestName (ServicePath);
+#if DEBUG_DBUS
 			Console.WriteLine("NameReply {0}", nameReply);
 #endif
 			Bus.Session.Register (ServicePath, CorePath, this);
