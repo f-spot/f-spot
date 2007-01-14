d6ab7188e521762ed3b62ef3d12a23aa8601437e
Larry Ewing <lewing@novell.com>
2007-01-14 18:10:16 +0000

    add a horribly hacked up Tao build to the repo. So many wrappers..
    2007-01-14  Larry Ewing  <lewing@novell.com>
    
            * Tao: add a horribly hacked up Tao build to the repo.  So many
            wrappers..
    
    
    svn path=/trunk/; revision=2725

---
+++ am-config.mk
@@ -0,0 +1,16 @@
+LIBRARY_DEST	= $(DESTDIR)/lib
+BINDIR		= $(DESTDIR)/bin
+ASSEMBLY_DLL	= $(LIBRARY).dll
+ASSEMBLY_EXE	= $(PROGRAM).exe
+ASSEMBLY_WINEXE	= $(PROGRAM_WIN).exe
+ASSEMBLY_NAME	= $(LIBRARY)
+SYMBOLS		= 
+CSFLAGS		= /unsafe
+
+# If ASSEMBLY_DLL has been set, and thus, is not ".dll", assume we're a 
+# library, and not an executable
+TARGET	= library
+ASSEMBLY	= $(ASSEMBLY_DLL)
+
+GACUTIL_FLAGS = 
+RUN_EXE = mono
