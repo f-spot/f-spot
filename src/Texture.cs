4cb7a32d54dee04a7623900ea117c3aa8ceeddf5
Larry Ewing <lewing@novell.com>
2007-01-17 21:59:18 +0000

    first pass at fixing the texture allocation issues.
    2007-01-17  Larry Ewing  <lewing@novell.com>
    
            * src/Texture.cs: first pass at fixing the texture allocation
            issues.
    
    
    svn path=/trunk/; revision=2775

--- src/Texture.cs
+++ src/Texture.cs
@@ -100,7 +100,7 @@ namespace FSpot {
 		{
 			this.width = width;
 			this.height = height;
-		
+			
 			Gl.glGenTextures (1, out texture_id);
 			//System.Console.WriteLine ("generated texture.{0} ({1}, {2})", texture_id, width, height); 
 		}
@@ -119,14 +119,38 @@ namespace FSpot {
 		{
 			Bind ();
 
-			if (surface.Width != width || surface.Height != height)
-				throw new TextureException ("Bad Surface Match");
-
+			int max_size;
+			Gl.glGetIntegerv (Gl.GL_MAX_RECTANGLE_TEXTURE_SIZE_ARB, out max_size);
+			float scale = (float)Math.Min (1.0, max_size / (double) Math.Max (width, height));
+			System.Console.WriteLine ("max texture size {0} scaling to {1}", max_size, scale);
+			
 			if (surface.Pixels == IntPtr.Zero)
 				throw new TextureException ("Surface has no data");
 
 			if (surface.Format != Format.Rgb24 && surface.Format != Format.Argb32)
 				throw new TextureException ("Unsupported format type");
+
+			IntPtr pixels = surface.Pixels;
+			IntPtr tmp = IntPtr.Zero;
+			if (scale != 1.0) {
+				int swidth = (int)(width * scale);
+				int sheight = (int) (height * scale);
+				tmp = Marshal.AllocHGlobal (swidth * sheight * 4);
+				System.Console.WriteLine ("scaling image {0} x {1}", swidth, sheight);
+
+				Glu.gluScaleImage (Gl.GL_BGRA,
+						  width,
+						  height,
+						  Gl.GL_UNSIGNED_BYTE,
+						  pixels,
+						  swidth,
+						  sheight,
+						  Gl.GL_UNSIGNED_BYTE,
+						  tmp);
+				pixels = tmp;
+				width = swidth;
+				height = sheight;
+			}
 			
 			Gl.glTexImage2D (Gl.GL_TEXTURE_RECTANGLE_ARB,
 					 0,
@@ -136,9 +160,14 @@ namespace FSpot {
 					 0,
 					 Gl.GL_BGRA,
 					 Gl.GL_UNSIGNED_BYTE,
-					 surface.Pixels);
+					 pixels);
 			
-			//			if (Gl.glGetError () != Gl.GL_NO_ERROR)
+
+			if (tmp != IntPtr.Zero)
+				Marshal.FreeHGlobal (tmp);
+
+			if (Gl.glGetError () != Gl.GL_NO_ERROR)
+				System.Console.WriteLine ("unable to allocate texture");
 			//	throw new TextureException ("Unable to allocate texture resources");
 
 			return texture_id;
