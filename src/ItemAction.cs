6a23825cb1c4cb777ec397e61765b64ca4a45ddf
Larry Ewing <lewing@src.gnome.org>
2007-01-20 23:04:59 +0000

    Create a new FilterAction as a subclass of ItemAction that takes a filter and an item and does the proper version creation and change notification through BrowsableItem.  Makes it possible to turn any filter into an action.
    svn path=/trunk/; revision=2809

--- src/ItemAction.cs
+++ src/ItemAction.cs
@@ -11,6 +11,8 @@
  */
 using Gtk;
 using Mono.Unix;
+using FSpot.Filters;
+using System;
 
 namespace FSpot {
 	public abstract class ItemAction : Action {
@@ -122,6 +124,124 @@ namespace FSpot {
 		protected override void OnActivated ()
 		{
 			item.MovePrevious ();
+		}
+	}
+
+	// FIXME this class is a hack to work around the considerable brokeness
+	// in the gaps between Photo and IBrowsable* It helps but it shouldn't
+	// be so introspective.
+	internal class EditTarget {
+		BrowsablePointer item;
+		bool created_version = false;
+		uint version;
+		Photo photo;
+		
+		public EditTarget (BrowsablePointer item)
+		{
+			this.item = item;
+			photo = item.Current as Photo;
+			if (photo != null) {
+				version = photo.DefaultVersionId;
+				bool create = version == Photo.OriginalVersionId;
+				
+				if (create) {
+					version = photo.CreateDefaultModifiedVersion (photo.DefaultVersionId, false);
+					created_version = true;
+				}
+			}
+		}
+		
+		public Uri Uri {
+			get {
+				if (photo != null)
+					return photo.VersionUri (version);
+				else 
+					return item.Current.DefaultVersionUri;
+			}
+		}
+		
+		public void Commit ()
+		{
+			PhotoQuery q = item.Collection as PhotoQuery;
+			if (photo != null && q != null) {
+				photo.DefaultVersionId = version;
+				q.Commit (item.Index);
+			} else {
+				item.Collection.MarkChanged (item.Index);
+			}
+		}
+		
+		public void Delete ()
+		{
+			if (created_version)
+				photo.DeleteVersion (version);
+		}
+		
+	}
+	
+	public class FilterAction : ItemAction {
+		public FilterAction (BrowsablePointer pointer,
+				     string name,
+				     string label,
+				     string tooltip,
+				     string stock_id) : base (pointer, name, label, tooltip, stock_id)
+		{
+		}
+
+		protected virtual IFilter BuildFilter ()
+		{
+			throw new ApplicationException ("No filter specified");
+		}
+		
+
+		protected override void OnActivated ()
+		{
+			if (!item.IsValid)
+				throw new ApplicationException ("attempt to filter invalid item");
+			
+		        using (FilterRequest req = new FilterRequest (item.Current.DefaultVersionUri)) {
+				IFilter filter = BuildFilter ();
+				if (filter.Convert (req)) {
+					// The filter did something so lets ve
+					EditTarget target = new EditTarget (item);
+					
+					Gnome.Vfs.Result result = Gnome.Vfs.Result.Ok;
+					result = Gnome.Vfs.Xfer.XferUri (new Gnome.Vfs.Uri (req.Current.ToString ()),
+									 new Gnome.Vfs.Uri (target.Uri.ToString ()),
+									 Gnome.Vfs.XferOptions.Default,
+									 Gnome.Vfs.XferErrorMode.Abort, 
+									 Gnome.Vfs.XferOverwriteMode.Replace, 
+									 delegate {
+										 System.Console.WriteLine ("progress");
+										 return 1;
+									 });
+					
+					if (result == Gnome.Vfs.Result.Ok) {
+						System.Console.WriteLine ("Done modifying image");
+						target.Commit ();
+					} else {
+						target.Delete ();
+						throw new ApplicationException (String.Format (
+											       "{0}: error moving to destination {1}",
+											       this, target.ToString ()));
+					}
+				}
+			}
+		}
+	}
+
+	public class AutoColor : FilterAction {
+		public AutoColor (BrowsablePointer p)
+			: base (p, "Color", 
+				Catalog.GetString ("Auto Color"),
+				Catalog.GetString ("Automatically adjust the colors"),
+				"f-spot-sepia")
+		{
+		}
+
+		protected override IFilter BuildFilter ()
+		{
+			return new AutoStretch ();
 		}
 	}
 }
