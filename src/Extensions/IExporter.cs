79a3430dba88c8f7af253eda6ba514902ae9d82f
Stephane Delcroix <stephane@delcroix.org>
2007-04-17 11:37:26 +0000

    move ExportMenuItemNode class in its own file, so IExporter can be linked
    2007-04-17  Stephane Delcroix  <stephane@delcroix.org>
    
    	* src/Makefile.am:
    	* src/Extensions.cs:
    	* src/ExportMenuItemNode.cs: move ExportMenuItemNode class in its own
    	file, so IExporter can be linked as is from the documentation.
    
    svn path=/trunk/; revision=3078

--- src/Extensions/IExporter.cs
+++ src/Extensions/IExporter.cs
@@ -17,24 +17,4 @@ namespace FSpot.Extensions
 	{
 		void Run (IBrowsableCollection selection);
 	}
-
-	public delegate FSpot.PhotoArray SelectedImages ();
-
-	[ExtensionNode ("ExportMenuItem")]
-	public class ExportMenuItemNode : MenuItemNode
-	{
-
-		[NodeAttribute ("command_type", true)]
-		string command_type;
-
-		public static SelectedImages SelectedImages;
-
-		protected override void OnActivated (object o, EventArgs e)
-		{
-			IExporter exporter = (IExporter) Addin.CreateInstance (command_type);
-			exporter.Run (SelectedImages ());
-		}
-	}
-
-
 }
