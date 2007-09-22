ae7bea193cfa49f152f11fe7504378f41d9384d8
Gabriel Burt <gabriel.burt@gmail.com>
2007-09-22 17:42:25 +0000

    Ported selection code from nautilus, you can now drag or right click while
    2007-09-22  Gabriel Burt  <gabriel.burt@gmail.com>
    
    	* src/Widgets/SaneTreeView.cs: Ported selection code from nautilus, you
    	can now drag or right click while having multiple rows selected and things
    	will behave correctly.
    
    svn path=/trunk/; revision=3389

--- src/Widgets/SaneTreeView.cs
+++ src/Widgets/SaneTreeView.cs
@@ -1,9 +1,9 @@
 /*
- * Widgets/SaneTreeView.cs: A Gtk# TreeView that doesn't clear your selection
- * if you right click (and in the future when you drag).
+ * Widgets/SaneTreeView.cs: A Gtk# TreeView that modifies the selection appropriately
+ * when you right click or drag.  Code ported from nautilus, src/file-manager/fm-list-view.c, r13045.
  *
- * Author(s)
- *   Gabriel Burt  <gabriel.burt@gmail.com>
+ * If you have multiple rows selected and right click or drag on one of them, in a normal TreeView
+ * your selection would change to just the row you clicked/dragged on.
  *
  * This is free software. See COPYING for details.
  */
@@ -15,37 +15,83 @@ namespace FSpot.Widgets
 {
     public class SaneTreeView : TreeView
     {
-        public TreePath PathAtPoint(double x, double y) {
+        protected bool row_selected_on_button_down, ignore_button_release, drag_started;
+
+        public SaneTreeView(TreeStore store) : base(store)
+        {
+            Selection.Mode = SelectionMode.Multiple;
+        }
+
+        public TreePath PathAtPoint(double x, double y)
+        {
             TreePath path_at_pointer = null;
             GetPathAtPos((int) x, (int) y, out path_at_pointer);
             return path_at_pointer;
         }
 
-        // Override the default ButtonPressEvent because it is stupid and
-        // returns true, meaning if you want to have another handler for the ButtonPress
-        // event you have to Glib.ConnectBefore.  Plus you usually want the default one to
-        // run first so it will select/unselect anything necessary before popping up a menu, say.
         protected override bool OnButtonPressEvent (Gdk.EventButton button)
         {
-            TreePath path_at_point = PathAtPoint (button.X, button.Y);
-            bool selected = (path_at_point == null) ? false : Selection.PathIsSelected (path_at_point);
+            bool call_parent = true;
+            drag_started = false;
+            TreePath path = PathAtPoint (button.X, button.Y);
+            row_selected_on_button_down = (path == null) ? false : Selection.PathIsSelected (path);
 
-            if (button.Button != 3 || !selected) {
-                base.OnButtonPressEvent (button);
+            if (button.Button == 3 && row_selected_on_button_down) {
+                call_parent = false;
+            } else if ((button.Button == 1 || button.Button == 2) &&
+                    ((button.State & ModifierType.ControlMask) != 0 || (button.State & ModifierType.ShiftMask) == 0)) {
+                if (row_selected_on_button_down) {
+                    call_parent = false;
+                } else if ((button.State & ModifierType.ControlMask) != 0) {
+                    call_parent = false;
+                    Selection.SelectPath (path);
+                }
             }
+
+            if (call_parent)
+                base.OnButtonPressEvent (button);
 
             return false;
         }
 
         protected override bool OnButtonReleaseEvent (Gdk.EventButton button)
         {
+            if (!drag_started) {
+                DidNotDrag (button);
+            }
+
             base.OnButtonReleaseEvent (button);
             return false;
         }
 
-        public SaneTreeView(TreeStore store) : base(store)
+        protected override void OnDragBegin (Gdk.DragContext context)
         {
-            Selection.Mode = SelectionMode.Multiple;
+            drag_started = true;
+            base.OnDragBegin(context);
+        }
+
+        protected void DidNotDrag(Gdk.EventButton button)
+        {
+            TreePath path = PathAtPoint (button.X, button.Y);
+
+            if (path != null) {
+                if ((button.Button == 1 || button.Button == 2)
+                    && ((button.State & ModifierType.ControlMask) != 0 ||
+                    (button.State & ModifierType.ShiftMask) == 0)
+                    && row_selected_on_button_down) {
+                    if (!ButtonEventModifiesSelection (button)) {
+                        Selection.UnselectAll ();
+                        Selection.SelectPath (path);
+                    } else {
+                        Selection.UnselectPath (path);
+                    }
+                }
+            }
+        }
+
+        protected static bool ButtonEventModifiesSelection (Gdk.EventButton button)
+        {
+            return (button.State & (ModifierType.ControlMask | ModifierType.ShiftMask)) != 0;
         }
     }
 }
