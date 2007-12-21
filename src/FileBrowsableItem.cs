e684e5d9396204da145942dc158472a196a8f7f4
Stephane Delcroix <sdelcroix@src.gnome.org>
2007-12-21 09:16:57 +0000

    adding the files
    svn path=/trunk/; revision=3519

---
+++ src/FileBrowsableItem.cs
@@ -0,0 +1,89 @@
+/*
+ * FileBrowsableItem.cs
+ *
+ * Author(s):
+ *	Larry Ewing  (lewing@novell.com)
+ *	Stephane Delcroix  (stephane@delcroix.org)
+ *
+ * This is free software. See COPYING for details
+ */
+
+
+using System;
+using System.IO;
+using System.Collections;
+using System.Xml;
+
+namespace FSpot {
+	public class FileBrowsableItem : IBrowsableItem, IDisposable
+	{
+		ImageFile img;
+		Uri uri;
+		bool attempted;
+
+		public FileBrowsableItem (Uri uri)
+		{
+			this.uri = uri;
+		}
+
+		public FileBrowsableItem (string path)
+		{
+			this.uri = UriList.PathToFileUri (path);
+		}
+
+		protected ImageFile Image {
+			get {
+				if (!attempted) {
+					img = ImageFile.Create (uri);
+					attempted = true;
+				}
+
+				return img;
+			}
+		}
+
+		public Tag [] Tags {
+			get {
+				return null;
+			}
+		}
+
+		public DateTime Time {
+			get {
+				return Image.Date;
+			}
+		}
+
+		public Uri DefaultVersionUri {
+			get {
+				return uri;
+			}
+		}
+
+		public string Description {
+			get {
+				try {
+					if (Image != null)
+						return Image.Description;
+
+				} catch (System.Exception e) {
+					System.Console.WriteLine (e);
+				}
+
+				return null;
+			}
+		}
+
+		public string Name {
+			get {
+				return Path.GetFileName (Image.Uri.AbsolutePath);
+			}
+		}
+
+		public void Dispose ()
+		{
+			img.Dispose ();
+			GC.SuppressFinalize (this);
+		}
+	}
+}
