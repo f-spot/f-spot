3e0b76dbedba1b28785da36055e5bd081128ced4
Stephane Delcroix <sdelcroix@src.gnome.org>
2008-01-16 20:48:40 +0000

    missing file
    svn path=/trunk/; revision=3558

---
+++ src/Query/LogicalTerm.cs
@@ -0,0 +1,16 @@
+/*
+ * FSpot.Query.LogicalTerm
+ *
+ * Author(s):
+ *	Stephane Delcroix  <stephane@delcroix.org>
+ *
+ * This is free software. See COPYING for details.
+ */
+
+namespace FSpot.Query
+{
+	public class LogicalTerm
+	{
+		
+	}
+}
