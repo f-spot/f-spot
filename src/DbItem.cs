74f9495f874c565c8c29c90a5455a3460dfe1aec
Stephane Delcroix <sdelcroix@src.gnome.org>
2008-01-18 13:38:00 +0000

    missing file
    svn path=/trunk/; revision=3571

---
+++ src/DbItem.cs
@@ -0,0 +1,21 @@
+/*
+ * FSpot.DbItem.cs
+ *
+ * Author(s):
+ *	Larry Ewing
+ *
+ * This is fre software. See COPYING for details.
+ */
+
+public class DbItem {
+	uint id;
+	public uint Id {
+		get { return id; }
+	}
+
+	protected DbItem (uint id) {
+		this.id = id;
+	}
+}
+
+
