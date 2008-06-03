c861b5c9a34ff0e94ef024402e12aa5d30d5c569
Stephane Delcroix <sdelcroix@src.gnome.org>
2008-06-03 11:29:55 +0000

    some more cleanup
    svn path=/trunk/; revision=4031

--- src/Utils/GdkUtils.cs
+++ src/Utils/GdkUtils.cs
@@ -5,46 +5,48 @@ using System.Runtime.InteropServices;
 namespace FSpot.Utils {
 	public class GdkUtils {
 
-		[DllImport("libgdk-2.0-0.dll")]
-		static extern uint gdk_x11_drawable_get_xid (IntPtr d);
-
-		[DllImport("libgdk-2.0-0.dll")]
-		static extern IntPtr gdk_x11_display_get_xdisplay (IntPtr d);
-
-		[DllImport("libgdk-2.0-0.dll")]
-		static extern IntPtr gdk_x11_visual_get_xvisual (IntPtr d);
-
-		[DllImport("X11")]
-		internal static extern uint XVisualIDFromVisual(IntPtr visual);
-
-		[DllImport("libgdk-2.0-0.dll")]
-		static extern IntPtr gdk_x11_screen_lookup_visual (IntPtr screen,
-								   uint   xvisualid);
-
+		class NativeMethods
+		{
+			[DllImport("libgdk-2.0-0.dll")]
+			public static extern uint gdk_x11_drawable_get_xid (IntPtr d);
+	
+			[DllImport("libgdk-2.0-0.dll")]
+			public static extern IntPtr gdk_x11_display_get_xdisplay (IntPtr d);
+	
+			[DllImport("libgdk-2.0-0.dll")]
+			public static extern IntPtr gdk_x11_visual_get_xvisual (IntPtr d);
+	
+			[DllImport("X11")]
+			public static extern uint XVisualIDFromVisual(IntPtr visual);
+	
+			[DllImport("libgdk-2.0-0.dll")]
+			public static extern IntPtr gdk_x11_screen_lookup_visual (IntPtr screen,
+									   uint   xvisualid);
+		}
 
 		public static uint GetXid (Drawable d)
 		{
-			return gdk_x11_drawable_get_xid (d.Handle);
+			return NativeMethods.gdk_x11_drawable_get_xid (d.Handle);
 		}
 
 		public static uint GetXVisualId (Visual visual)
 		{
-			return XVisualIDFromVisual (GetXVisual (visual));
+			return NativeMethods.XVisualIDFromVisual (GetXVisual (visual));
 		}
 		
 		public static IntPtr GetXDisplay (Display display)
 		{
-			return gdk_x11_display_get_xdisplay (display.Handle);
+			return NativeMethods.gdk_x11_display_get_xdisplay (display.Handle);
 		}
 		
 		public static IntPtr GetXVisual (Visual v)
 		{
-			return gdk_x11_visual_get_xvisual (v.Handle);
+			return NativeMethods.gdk_x11_visual_get_xvisual (v.Handle);
 		}
 
 		public static Visual LookupVisual (Screen screen, uint visualid)
 		{
-			return (Gdk.Visual) GLib.Object.GetObject (gdk_x11_screen_lookup_visual (screen.Handle, visualid));
+			return (Gdk.Visual) GLib.Object.GetObject (NativeMethods.gdk_x11_screen_lookup_visual (screen.Handle, visualid));
 		}
 	}
 }
