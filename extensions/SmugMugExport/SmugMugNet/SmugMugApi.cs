ca8ba33aa5886eb57f2a2763bb373c2960755b4c
Stephane Delcroix <sdelcroix@src.gnome.org>
2008-06-05 15:57:53 +0000

    fix missing using
    svn path=/trunk/; revision=4047

--- extensions/SmugMugExport/SmugMugNet/SmugMugApi.cs
+++ extensions/SmugMugExport/SmugMugNet/SmugMugApi.cs
@@ -14,6 +14,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Net;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Xml;
 using System.Collections.Specialized;
 
