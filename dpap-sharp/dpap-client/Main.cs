f6777424ec4c5a12466bdb0adc561a31618cfd5e
Andrzej Wytyczak-Partyka <apart@src.gnome.org>
2008-07-22 15:45:10 +0000

    Fixed license, copyright & author information
    svn path=/trunk/; revision=4195

--- dpap-sharp/dpap-client/Main.cs
+++ dpap-sharp/dpap-client/Main.cs
@@ -1,7 +1,24 @@
-// Main.cs created with MonoDevelop
-// User: andrzej at 10:26 2008-06-01
+// Main.cs
+// dpap-sharp client test program
+// Author:
+//   Andrzej Wytyczak-Partyka <iapart@gmail.com>
 //
-// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+// Copyright (C) 2008 Andrzej Wytyczak-Partyka
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
 //
 using System;
 using System.IO;
