c42bbf63a1ae73cef44505dddeadee34bdd2637a
Stephane Delcroix <sdelcroix@novell.com>
2009-02-12 14:32:46 +0000

    move the tests to Test/
    2009-02-12  Stephane Delcroix  <sdelcroix@novell.com>
    
    	* src/Imaging/JpegFile.cs: move the tests to Test/
    
    svn path=/trunk/; revision=4707

--- src/Imaging/JpegFile.cs
+++ src/Imaging/JpegFile.cs
@@ -4,10 +4,6 @@ using System.IO;
 using FSpot.Xmp;
 using FSpot.Tiff;
 
-#if ENABLE_NUNIT
-using NUnit.Framework;
-#endif
-
 namespace FSpot {
 	public interface IThumbnailContainer {
 		Gdk.Pixbuf GetEmbeddedThumbnail ();
@@ -348,48 +344,5 @@ namespace FSpot {
 			}
 		}
 
-#if ENABLE_NUNIT
-		[TestFixture]
-		public class Tests {
-			public Tests ()
-			{
-				Gnome.Vfs.Vfs.Initialize ();
-				Gtk.Application.Init ();
-			}
-			
-#if false
-			[Test]
-			public void TestLoad ()
-			{
-				JpegFile jimg = new JpegFile ("/home/lewing/start.swe.jpeg");
-				Assert.AreEqual (PixbufOrientation.TopLeft, jimg.Orientation);
-			}
-#endif
-			[Test]
-			public void TestSave ()
-			{
-				string desc = "this is an example description";
-				string desc2 = "\x00a9 Novell Inc.";
-				PixbufOrientation orient = PixbufOrientation.TopRight;
-				Gdk.Pixbuf test = new Gdk.Pixbuf (null, "f-spot-32.png");
-				string path = ImageFile.TempPath ("joe.jpg");
-				
-				PixbufUtils.SaveJpeg (test, path, 75, new Exif.ExifData ());
-				JpegFile jimg = new JpegFile (path);
-				jimg.SetDescription (desc);
-				jimg.SetOrientation (orient);
-				jimg.SaveMetaData (path);
-				JpegFile mod = new JpegFile (path);
-				Assert.AreEqual (mod.Orientation, orient);
-				Assert.AreEqual (mod.Description, desc);
-				jimg.SetDescription (desc2);
-				jimg.SaveMetaData (path);
-				mod = new JpegFile (path);
-				Assert.AreEqual (mod.Description, desc2);
-				
-				File.Delete (path);
-			}
-		}
-#endif
 	}
 }
