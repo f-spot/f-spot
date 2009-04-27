dbbf60b42a0220516948015160cf6dd271ee1146
Philip Withnall <philip@tecnocode.co.uk>
2009-04-27 18:08:00 +0100

    Bug 578528 – Add date query D-Bus method
    The QueryByDate method takes two longs, interpreted as Unix timestamps for the
    start and end of a date range. Patch reviewed by Stephane Delcroix.

--- extensions/Services/DBusService/DBusProxy.cs
+++ extensions/Services/DBusService/DBusProxy.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Text;
 
 using FSpot;
+using FSpot.Query;
+using FSpot.Utils;
 using NDesk.DBus;
 
 namespace DBusService {
@@ -276,6 +278,7 @@ namespace DBusService {
 
 		// query
 		uint[] Query (string []tags);
+		uint[] QueryByDate (long start_time, long end_time);
 
 		// events
 		event RemoteUp RemoteUp;
@@ -345,6 +348,20 @@ namespace DBusService {
 
 			Photo []photos = db.Photos.Query (tag_list.ToArray ());
 
+			uint []ids = new uint[photos.Length];
+
+			for (int i = 0; i < ids.Length; i++)
+				ids[i] = photos[i].Id;
+
+			return ids;
+		}
+
+		public uint[] QueryByDate (long start_time, long end_time)
+		{
+			DateTime start = DbUtils.DateTimeFromUnixTime (start_time);
+			DateTime end = DbUtils.DateTimeFromUnixTime (end_time);
+
+			Photo []photos = db.Photos.Query (new DateRange (start, end));
 			uint []ids = new uint[photos.Length];
 
 			for (int i = 0; i < ids.Length; i++)
