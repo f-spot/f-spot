48a2ccb7856868cf5eae20a38970f5fc514554b3
Wojciech Dzierżanowski <wojciech.dzierzanowski@gmail.com>
2009-06-09 13:34:00 +0200

    From 7bbf4f3a8d87b7cb72cdb4153ace6b9487ab4acf Mon Sep 17 00:00:00 2001 From: =?utf-8?q?Wojciech=20Dzier=C5=BCanowski?= Author: Wojciech Dzierżanowski <wojciech.dzierzanowski@gmail.com> <wojciech.dzierzanowski@gmail.com> Date: Mon, 11 May 2009 23:09:31 +0200 Subject: [PATCH] TabbloExport refactorings and bugfixes
       * `Mono.Tabblo' namespace now uses `System.Diagnostics' for logging.
         `Mono.Tabblo' must not depend on F-Spot.  When `Mono.Tabblo' runs
    within
         F-Spot, its diagnostic output still goes through `FSpot.Utils.Log'
    thanks
         to a listener attached by `TabbloExport' to
    `System.Diagnostics.Debug'.
       * `FSpotTabbloExport' namespace adopted an MVC design.
       * Some other minor refactorings.
       * Fixed bug: The "Export" button should have the initial input focus.
       * Fixed bug: Work around tabblo.com's  way of determining the
       * expected
         image file format.  tabblo.com expects the file type to match the
         file extension specified as one of the POST arguments.
    TabbloExport
         used to set this argument to the name of the original file rather
    than
         the version actually being exported.  This caused trouble e.g. when
         exporting photos developed from RAW with DevelopInUFRaw.
       * Bumped version number.

--- extensions/Exporters/TabbloExport/TabbloExport.cs
+++ extensions/Exporters/TabbloExport/TabbloExport.cs
@@ -101,6 +101,25 @@ namespace FSpotTabbloExport {
 			model.PasswordChanged += HandlePasswordChanged;
 			main_dialog.password_entry.Changed +=
 					HandlePasswordChanged;
+
+			// Tags
+			model.AttachTagsChanged += HandleAttachTagsToggled;
+			main_dialog.attach_tags_button.Toggled +=
+					HandleAttachTagsToggled;
+
+			model.AttachedTagsChanged += HandleAttachedTagsChanged;
+
+			main_dialog.attached_tags_select_button.Clicked +=
+					HandleSelectAttachedTagsClicked;
+
+			model.RemoveTagsChanged += HandleRemoveTagsToggled;
+			main_dialog.remove_tags_button.Toggled +=
+					HandleRemoveTagsToggled;
+
+			model.RemovedTagsChanged += HandleRemovedTagsChanged;
+
+			main_dialog.removed_tags_select_button.Clicked +=
+					HandleSelectRemovedTagsClicked;
 		}
 
 
@@ -135,6 +154,107 @@ namespace FSpotTabbloExport {
 			main_dialog.Validated =
 					model.Username.Length > 0
 					&& model.Password.Length > 0;
+		}
+
+
+		private void HandleAttachTagsToggled (object sender,
+		                                      EventArgs args)
+		{
+			if (model == sender) {
+				main_dialog.attach_tags_button.Active =
+						model.AttachTags;
+				main_dialog.attached_tags_select_button
+						.Sensitive = model.AttachTags;
+
+				if (model.AttachTags && 0 == model
+							.AttachedTags.Length) {
+					model.AttachedTags = SelectTags ();
+				}
+			} else {
+				model.AttachTags =
+					main_dialog.attach_tags_button.Active;
+			}
+		}
+
+		private void HandleRemoveTagsToggled (object sender,
+		                                      EventArgs args)
+		{
+			if (model == sender) {
+				main_dialog.remove_tags_button.Active =
+						model.RemoveTags;
+				main_dialog.removed_tags_select_button
+						.Sensitive = model.RemoveTags;
+
+				if (model.RemoveTags && 0 == model
+							.RemovedTags.Length) {
+					model.RemovedTags = SelectTags ();
+				}
+			} else {
+				model.RemoveTags =
+					main_dialog.remove_tags_button.Active;
+			}
+		}
+
+
+		private void HandleSelectAttachedTagsClicked (object sender,
+		                                              EventArgs args)
+		{
+			Debug.Assert (model != sender);
+
+			FSpot.Tag [] tags = SelectTags ();
+			if (null != tags) {
+				model.AttachedTags = tags;
+			}
+		}
+
+		private void HandleSelectRemovedTagsClicked (object sender,
+		                                             EventArgs args)
+		{
+			Debug.Assert (model != sender);
+
+			FSpot.Tag [] tags = SelectTags ();
+			if (null != tags) {
+				model.RemovedTags = tags;
+			}
+		}
+
+
+		private void HandleAttachedTagsChanged (object sender,
+		                                        EventArgs args)
+		{
+			Debug.Assert (model == sender);
+
+			main_dialog.attached_tags_view.Tags =
+					model.AttachedTags;
+			main_dialog.attach_tags_button.Active =
+					model.AttachTags
+					&& model.AttachedTags.Length > 0;
+		}
+
+		private void HandleRemovedTagsChanged (object sender,
+		                                       EventArgs args)
+		{
+			Debug.Assert (model == sender);
+
+			main_dialog.removed_tags_view.Tags =
+					model.RemovedTags;
+			main_dialog.remove_tags_button.Active =
+					model.RemoveTags
+					&& model.RemovedTags.Length > 0;
+		}
+
+
+		private FSpot.Tag [] SelectTags ()
+		{
+			TagStore tag_store = FSpot.Core.Database.Tags;
+			FSpot.UI.Dialog.TagSelectionDialog tagDialog =
+					new FSpot.UI.Dialog.TagSelectionDialog (
+							tag_store);
+			FSpot.Tag [] tags = tagDialog.Run ();
+
+			tagDialog.Hide ();
+
+			return tags;
 		}
 
 
@@ -241,6 +361,26 @@ namespace FSpotTabbloExport {
 		private void OnPhotoUploaded (FSpot.IBrowsableItem item)
 		{
 			Debug.Assert (null != item);
+
+			if (!model.AttachTags && !model.RemoveTags) {
+				return;
+			}
+
+			PhotoStore photo_store = FSpot.Core.Database.Photos;
+			FSpot.Photo photo = photo_store.GetByUri (
+					item.DefaultVersionUri);
+			Debug.Assert (null != photo);
+			if (null == photo) {
+				return;
+			}
+
+			if (model.AttachTags) {
+				photo.AddTag (model.AttachedTags);
+			}
+			if (model.RemoveTags) {
+				photo.RemoveTag (model.RemovedTags);
+			}
+			photo_store.Commit (photo);
 		}
 
 		
