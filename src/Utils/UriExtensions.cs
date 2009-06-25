db23918845eb70d84e550fcf06802bcaa1dbafcc
Mike Gemünde <mike@gemuende.de>
2009-06-25 20:56:46 +0200

    Uri extensions methods
    extensions methods to get the "folder" like part and the "filename" like part.

---
+++ src/Utils/UriExtensions.cs
@@ -0,0 +1,34 @@
+/*
+ * FSpot.Utils.UriExtensions.cs
+ *
+ * Author(s)
+ * 	Mike Gemuende <mike@gemuende.de>
+ *
+ * This is free software. See COPYING for details.
+ */
+
+using System;
+
+namespace FSpot.Utils
+{
+	
+	public static class UriExtensions
+	{
+		public static Uri GetDirectoryUri (this Uri uri)
+		{
+			UriBuilder builder = new UriBuilder (uri);
+			builder.Path =
+				String.Format ("{0}/", System.IO.Path.GetDirectoryName (uri.AbsolutePath));
+			
+			return builder.Uri;
+		}
+		
+		public static string GetFilename (this Uri uri)
+		{
+			if ( ! uri.IsFile)
+				return null;
+			
+			return System.IO.Path.GetFileName (uri.AbsolutePath);
+		}
+	}
+}
