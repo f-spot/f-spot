2974d8e25dbb5f4bd9ecdcf0f9f6a0616060f1ab
Lorenzo Milesi <maxxer@yetopen.it>
2009-06-29 08:41:05 +0200

    Translation stuff
    Made ThumbnailCommand.cs translatable, fixed POTFILES.in for missing
    files.

--- src/ThumbnailCommand.cs
+++ src/ThumbnailCommand.cs
@@ -17,7 +17,7 @@ public class ThumbnailCommand {
 		ProgressDialog progress_dialog = null;
 
 		if (photos.Length > 1) {
-			progress_dialog = new ProgressDialog ("Updating Thumbnails",
+			progress_dialog = new ProgressDialog (Mono.Unix.Catalog.GetString ("Updating Thumbnails"),
 							      ProgressDialog.CancelButtonType.Stop,
 							      photos.Length, parent_window);
 		}
@@ -25,7 +25,7 @@ public class ThumbnailCommand {
 		int count = 0;
 		foreach (Photo p in photos) {
 			if (progress_dialog != null
-			    && progress_dialog.Update (String.Format ("Updating picture \"{0}\"", p.Name)))
+			    && progress_dialog.Update (String.Format (Mono.Unix.Catalog.GetString ("Updating picture \"{0}\""), p.Name)))
 				break;
 
 			foreach (uint version_id in p.VersionIds) {
