31f01cd94d79f505f3f4ea4d5887f6927be17105
Ruben Vermeersch <ruben@savanne.be>
2009-07-08 14:41:07 +0100

    Split classes out into their own files.

---
+++ src/Loaders/AreaUpdatedEventArgs.cs
@@ -0,0 +1,28 @@
+//
+// Fspot.Loaders.AreaUpdatedEventArgs.cs
+//
+// Copyright (c) 2009 Novell, Inc.
+//
+// Author(s)
+//	Stephane Delcroix  <sdelcroix@novell.com>
+//
+// This is free software. See COPYING for details
+//
+
+using System;
+using Gdk;
+
+namespace FSpot.Loaders {
+	public class AreaUpdatedEventArgs : EventArgs
+	{
+		Gdk.Rectangle area;
+		public Gdk.Rectangle Area { 
+			get { return area; }
+		}
+
+		public AreaUpdatedEventArgs (Gdk.Rectangle area) : base ()
+		{
+			this.area = area;
+		}
+	}
+}
--- src/Loaders/IImageLoader.cs
+++ src/Loaders/IImageLoader.cs
@@ -4,7 +4,6 @@
 // Copyright (c) 2009 Novell, Inc.
 //
 // Author(s)
-//	Stephane Delcroix  <sdelcroix@novell.com>
 //	Ruben Vermeersch  <ruben@savanne.be>
 //
 // This is free software. See COPYING for details
@@ -15,33 +14,6 @@ using System;
 using Gdk;
 
 namespace FSpot.Loaders {
-	public class AreaPreparedEventArgs : EventArgs
-	{
-		bool reduced_resolution;
-
-		public bool ReducedResolution {
-			get { return reduced_resolution; }
-		}
-	
-		public AreaPreparedEventArgs (bool reduced_resolution) : base ()
-		{
-			this.reduced_resolution = reduced_resolution;
-		}
-	}
-
-	public class AreaUpdatedEventArgs : EventArgs
-	{
-		Gdk.Rectangle area;
-		public Gdk.Rectangle Area { 
-			get { return area; }
-		}
-
-		public AreaUpdatedEventArgs (Gdk.Rectangle area) : base ()
-		{
-			this.area = area;
-		}
-	}
-
 	public interface IImageLoader : IDisposable {
 		bool Loading { get; }	
 
