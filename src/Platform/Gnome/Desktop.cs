f700147602e90f5c653ccb0a6d713ac070b1cc36
Stephane Delcroix <stephane@delcroix.org>
2009-10-02 10:35:22 +0200

    zoom the images on setting the background

--- src/Platform/Gnome/Desktop.cs
+++ src/Platform/Gnome/Desktop.cs
@@ -16,7 +16,7 @@ namespace FSpot.Platform
 			GConf.Client client = new GConf.Client (); 
 			client.Set ("/desktop/gnome/background/color_shading_type", "solid");
 			client.Set ("/desktop/gnome/background/primary_color", "#000000");
-			client.Set ("/desktop/gnome/background/picture_options", "stretched");
+			client.Set ("/desktop/gnome/background/picture_options", "zoom");
 			client.Set ("/desktop/gnome/background/picture_opacity", 100);
 			client.Set ("/desktop/gnome/background/picture_filename", path);
 			client.Set ("/desktop/gnome/background/draw_background", true);
