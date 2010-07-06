a8d7950acfd799bb0c3b2637a43b355c988de986
Mike Gemünde <mike@gemuende.de>
2010-07-06 14:40:36 +0200

    Add SetDefaultVersion to IBrowsableItemVersionable
    This commit starts adding the behavior to IBrowsableItemVersionable
    which is related to an item with different versions where versions
    can be changed.

--- src/Core/IBrowsableItemVersionable.cs
+++ src/Core/IBrowsableItemVersionable.cs
@@ -1,8 +1,30 @@
+/*
+ * IBrowsableItemVersion.cs
+ * 
+ * Author(s):
+ *  Ruben Vermeersch <ruben@savanne.be>
+ *  Mike Gemuende <mike@gemuende.de>
+ *
+ * This is free software. See COPYING for details.
+ */
+
 using System.Collections.Generic;
 
 namespace FSpot
 {
-	public interface IBrowsableItemVersionable {
-		IEnumerable<IBrowsableItemVersion> Versions { get; }
+	/// <summary>
+	///    The interface adds functionality which is related to items where
+	///    versions can be added or removed.
+	/// </summary>
+	public interface IBrowsableItemVersionable : IBrowsableItem{
+
+		/// <summary>
+		///    Sets the default version of a the item.
+		/// </summary>
+		/// <param name="version">
+		///    A <see cref="IBrowsableItemVersion"/> which will be the new
+		///    default version.
+		/// </param>
+		void SetDefaultVersion (IBrowsableItemVersion version);
 	}
 }
