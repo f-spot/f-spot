fa7340743abcb91cffc1805af9d88be190c70866
Mike Gemünde <mike@gemuende.de>
2010-09-08 09:39:22 +0200

    Clean up PhotoList
    Make Constructors more generic, remove unused method AddAll() (which
    btw had a strange behavior) and reformat the file (whitespaces).

--- src/Core/FSpot.Core/FSpot.Core/PhotoList.cs
+++ src/Core/FSpot.Core/FSpot.Core/PhotoList.cs
@@ -2,117 +2,112 @@
  * FSpot.PhotoList.cs
  *
  * Author(s):
- *	Larry Ewing
+ *  Larry Ewing
  *
  * This is free software, See COPYING for details
  */
 
 using System.Collections.Generic;
 
-namespace FSpot.Core {
+namespace FSpot.Core
+{
 
-	public class PhotoList : IBrowsableCollection {
-		protected List<IPhoto> list;
-		IPhoto [] cache;
+    public class PhotoList : IBrowsableCollection
+    {
+        protected List<IPhoto> list;
+        private IPhoto[] cache;
 
-		public PhotoList (IPhoto [] photos)
-		{
-			list = new List<IPhoto> (photos);
-		}
+        public PhotoList (IEnumerable<IPhoto> photos)
+        {
+            list = new List<IPhoto> (photos);
+        }
 
-		public PhotoList ()
-		{
-			list = new List<IPhoto> ();
-		}
+        public PhotoList (params IPhoto[] photos) : this(photos as IEnumerable<IPhoto>)
+        {
+        }
 
-		public int Count {
-			get { return list.Count; }
-		}
+        public int Count {
+            get { return list.Count; }
+        }
 
-		public void Clear ()
-		{
-			list.Clear ();
-			Reload ();
-		}
+        public void Clear ()
+        {
+            list.Clear ();
+            Reload ();
+        }
 
-		public int Capacity {
-			set { list.Capacity = value; }
-		}
+        public int Capacity {
+            set { list.Capacity = value; }
+        }
 
-		public void AddAll (List<IPhoto> photos)
-		{
-			list = photos;
-			Reload ();
-		}
+        public void Add (IPhoto photo)
+        {
+            list.Add (photo);
+            Reload ();
+        }
 
-		public void Add (IPhoto photo)
-		{
-			list.Add (photo);
-			Reload ();
-		}
+        public void Add (IEnumerable<IPhoto> items)
+        {
+            list.AddRange (items);
+            Reload ();
+        }
 
-		public void Add (IPhoto [] items)
-		{
-			list.AddRange (items);
-			Reload ();
-		}
+        public int IndexOf (IPhoto item)
+        {
+            return list.IndexOf (item);
+        }
 
-		public int IndexOf (IPhoto item)
-		{
-			return list.IndexOf (item);
-		}
+        public bool Contains (IPhoto item)
+        {
+            return list.Contains (item);
+        }
 
-		public bool Contains (IPhoto item)
-		{
-			return list.Contains (item);
-		}
+        public IPhoto this[int index] {
+            get { return list[index]; }
+            set {
+                list[index] = value;
+                MarkChanged (index, FullInvalidate.Instance);
+            }
+        }
 
-		public IPhoto this [int index] {
-			get { return list [index]; }
-			set {
-				list [index] = value;
-				MarkChanged (index, FullInvalidate.Instance);
-			}
-		}
+        public void Sort (IComparer<IPhoto> compare)
+        {
+            list.Sort (compare);
+            Reload ();
+        }
 
-		public void Sort (IComparer<IPhoto> compare)
-		{
-			list.Sort (compare);
-			Reload ();
-		}
+        public void Reload ()
+        {
+            cache = null;
+            if (Changed != null)
+                Changed (this);
+        }
 
-		public void Reload ()
-		{
-			cache = null;
-			if (Changed != null)
-				Changed (this);
-		}
+        public void MarkChanged (int num, IBrowsableItemChanges changes)
+        {
+            MarkChanged (new BrowsableEventArgs (num, changes));
+        }
 
-		public void MarkChanged (int num, IBrowsableItemChanges changes)
-		{
-			MarkChanged (new BrowsableEventArgs (num, changes));
-		}
+        public void MarkChanged (BrowsableEventArgs args)
+        {
+            if (ItemsChanged != null)
+                ItemsChanged (this, args);
+        }
 
-		public void MarkChanged (BrowsableEventArgs args)
-		{
-			if (ItemsChanged != null)
-				ItemsChanged (this, args);
-		}
+        public IPhoto[] Items {
+            get {
+                if (cache == null)
+                    cache = list.ToArray ();
+                
+                return cache;
+            }
+            set {
+                list.Clear ();
+                Add (value);
+            }
+        }
 
-		public IPhoto [] Items {
-			get {
-				if (cache == null)
-					cache = list.ToArray ();
-
-				return cache;
-			}
-			set {
-				list.Clear ();
-				Add (value);
-			}
-		}
-
-		public event IBrowsableCollectionChangedHandler Changed;
-		public event IBrowsableCollectionItemsChangedHandler ItemsChanged;
-	}
+        public event IBrowsableCollectionChangedHandler Changed;
+        public event IBrowsableCollectionItemsChangedHandler ItemsChanged;
+    }
 }
