9e22fb3ccf56520c298fcc8bd95697c3d6f8adbd
Ruben Vermeersch <ruben@savanne.be>
2010-10-09 13:58:34 +0200

    Change license to MIT X11.

--- src/Core/FSpot.Gui/FSpot.Widgets/OpenWithMenu.cs
+++ src/Core/FSpot.Gui/FSpot.Widgets/OpenWithMenu.cs
@@ -1,17 +1,33 @@
-/*
- * FSpot.Widgets.OpenWithMenu.cs
- *
- * Author(s)
- * 	Stephane Delcroix  <stephane@delcroix.org>
- * 	Loz  <gnome2@flower.powernet.co.uk>
- * 	Gabriel Burt  <gabriel.burt@gmail.com>
- * 	Larry Ewing  <lewing@novell.com>
- *
- * Copyright (c) 2007 Stephane Delcroix
- * Copyright (c) 2007-2009 Novell, Inc.
- *
- * This is free software. See COPYING for details.
- */
+//
+// OpenWithMenu.cs
+//
+// Author:
+//   Stephane Delcroix <stephane@delcroix.org>
+//   Gabriel Burt <gabriel.burt@gmail.com>
+//
+// Copyright (C) 2006-2009 Novell, Inc.
+// Copyright (C) 2007-2009 Stephane Delcroix
+// Copyright (C) 2006 Gabriel Burt
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 using System;
 using System.Collections.Generic;
--- src/Core/FSpot.Utils/FSpot.Utils/FileExtensions.cs
+++ src/Core/FSpot.Utils/FSpot.Utils/FileExtensions.cs
@@ -1,11 +1,33 @@
-/*
- * FSpot.Utils.FileExtensions.cs
- *
- * Author(s)
- * 	Paul Wellner Bou <paul@purecodes.org>
- *
- * This is free software. See COPYING for details.
- */
+//
+// FileExtensions.cs
+//
+// Author:
+//   Ruben Vermeersch <ruben@savanne.be>
+//   Paul Wellner Bou <paul@purecodes.org>
+//
+// Copyright (C) 2010 Novell, Inc.
+// Copyright (C) 2010 Ruben Vermeersch
+// Copyright (C) 2010 Paul Wellner Bou
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
 using System;
 using System.IO;
