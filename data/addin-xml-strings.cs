5012117e8060ae6dc7edd80e0ad7f58a442d42b8
Ruben Vermeersch <ruben@savanne.be>
2010-10-21 13:53:26 +0200

    Add generated addin strings.

---
+++ data/addin-xml-strings.cs
@@ -0,0 +1,135 @@
+// Generated - Do Not Edit!
+
+internal static class AddinXmlStringCatalog
+{
+    private static void Strings ()
+    {
+        // ../src/Clients/MainApp/FSpot.addin.xml
+
+        // ../src/Extensions/Editors/FSpot.Editors.Blackout/Resources/BlackoutEditor.addin.xml
+        Catalog.GetString (@"BlackoutEditor");
+        Catalog.GetString (@"Blacks out an area of the image.");
+        Catalog.GetString (@"Editors");
+
+        // ../src/Extensions/Editors/FSpot.Editors.BW/Resources/BWEditor.addin.xml
+        Catalog.GetString (@"BWEditor");
+        Catalog.GetString (@"Convert to B/W with control. Require Mono 2.2.");
+        Catalog.GetString (@"Editors");
+
+        // ../src/Extensions/Editors/FSpot.Editors.Flip/Resources/FlipEditor.addin.xml
+        Catalog.GetString (@"FlipEditor");
+        Catalog.GetString (@"Flips the image horizontally.");
+        Catalog.GetString (@"Editors");
+
+        // ../src/Extensions/Editors/FSpot.Editors.Pixelate/Resources/PixelateEditor.addin.xml
+        Catalog.GetString (@"PixelateEditor");
+        Catalog.GetString (@"Pixelates an area of the image.");
+        Catalog.GetString (@"Editors");
+
+        // ../src/Extensions/Editors/FSpot.Editors.Resize/Resources/ResizeEditor.addin.xml
+        Catalog.GetString (@"ResizeEditor");
+        Catalog.GetString (@"Resize the image");
+        Catalog.GetString (@"Editors");
+
+        // ../src/Extensions/Exporters/FSpot.Exporters.CD/Resources/CDExport.addin.xml
+        Catalog.GetString (@"CD Export");
+        Catalog.GetString (@"This extension allows you to burn your photos to CD.");
+        Catalog.GetString (@"Export");
+
+        // ../src/Extensions/Exporters/FSpot.Exporters.Facebook/Resources/FacebookExport.addin.xml
+        Catalog.GetString (@"FacebookExport");
+        Catalog.GetString (@"Export pictures to facebook");
+        Catalog.GetString (@"Export");
+
+        // ../src/Extensions/Exporters/FSpot.Exporters.Flickr/Resources/FlickrExport.addin.xml
+        Catalog.GetString (@"Flickr Export");
+        Catalog.GetString (@"This extension allows you to export your photos to Flickr and 23hq.");
+        Catalog.GetString (@"Export");
+
+        // ../src/Extensions/Exporters/FSpot.Exporters.Folder/Resources/FolderExport.addin.xml
+        Catalog.GetString (@"Folder Export");
+        Catalog.GetString (@"This extension allows you to export your photos to HTML galleries, Original galleries or just to a folder.");
+        Catalog.GetString (@"Export");
+
+        // ../src/Extensions/Exporters/FSpot.Exporters.Gallery/Resources/GalleryExport.addin.xml
+        Catalog.GetString (@"Gallery Export");
+        Catalog.GetString (@"This extension allows you to export your photos to PHP Gallery (http://gallery.menalto.com).");
+        Catalog.GetString (@"Export");
+
+        // ../src/Extensions/Exporters/FSpot.Exporters.PicasaWeb/Resources/PicasaWebExport.addin.xml
+        Catalog.GetString (@"PicasaWeb Export");
+        Catalog.GetString (@"This extension allows you to export your photos to PicasaWeb.");
+        Catalog.GetString (@"Export");
+
+        // ../src/Extensions/Exporters/FSpot.Exporters.SmugMug/Resources/SmugMugExport.addin.xml
+        Catalog.GetString (@"SmugMug Export");
+        Catalog.GetString (@"This extension allows you to export your photos to SmugMug.");
+        Catalog.GetString (@"Export");
+
+        // ../src/Extensions/Exporters/FSpot.Exporters.Tabblo/Resources/TabbloExport.addin.xml
+        Catalog.GetString (@"Tabblo Export");
+        Catalog.GetString (@"This extension allows you to export your photos to Tabblo.");
+        Catalog.GetString (@"Export");
+
+        // ../src/Extensions/Exporters/FSpot.Exporters.Zip/Resources/ZipExport.addin.xml
+        Catalog.GetString (@"Zip export");
+        Catalog.GetString (@"Simple export to Zip file");
+        Catalog.GetString (@"Export");
+
+        // ../src/Extensions/Tools/FSpot.Tools.ChangePhotoPath/Resources/ChangePhotoPath.addin.xml
+        Catalog.GetString (@"ChangePath");
+        Catalog.GetString (@"UNSTABLE - Please ensure you have a backup of your photos.db before you test this extension. This extension will allow you to change the base path to the your photos. It is very handy if you move your photos from ~/Photos to /OurPhotos for instance. Just ensure you let SqLite continue to process the changes for up to a few hours after f-spot reports finished. It will only change the path to photos which are located under the Photo directory.");
+        Catalog.GetString (@"Tools");
+
+        // ../src/Extensions/Tools/FSpot.Tools.DevelopInUFraw/Resources/DevelopInUFRaw.addin.xml
+        Catalog.GetString (@"DevelopInUFRaw");
+        Catalog.GetString (@"Develop the image in UFRaw, saves the result as a new version\n\nNote: Require ufraw 0.13 or CVS version newer than 2007-09-06 !!!");
+        Catalog.GetString (@"Tools");
+
+        // ../src/Extensions/Tools/FSpot.Tools.LiveWebGallery/Resources/LiveWebGallery.addin.xml
+        Catalog.GetString (@"LiveWebGallery");
+        Catalog.GetString (@"Share photos with other people on the same network via Web Browser");
+        Catalog.GetString (@"Tools");
+
+        // ../src/Extensions/Tools/FSpot.Tools.MergeDb/Resources/MergeDb.addin.xml
+        Catalog.GetString (@"MergeDb");
+        Catalog.GetString (@"Merge another db back to the main one");
+        Catalog.GetString (@"Tools");
+
+        // ../src/Extensions/Tools/FSpot.Tools.MetaPixel/MetaPixel.addin.xml
+        Catalog.GetString (@"MetaPixel");
+        Catalog.GetString (@"Create photomosaics using MetaPixel (http://www.complang.tuwien.ac.at/schani/metapixel/)");
+        Catalog.GetString (@"Tools");
+
+        // ../src/Extensions/Tools/FSpot.Tools.PictureTile/PictureTile.addin.xml
+        Catalog.GetString (@"PictureTile");
+        Catalog.GetString (@"Create photo wall using PictureTile by Jamie Zawinski (http://www.jwz.org/picturetile/)");
+        Catalog.GetString (@"Tools");
+
+        // ../src/Extensions/Tools/FSpot.Tools.RawPlusJpeg/Resources/RawPlusJpeg.addin.xml
+        Catalog.GetString (@"RawPlusJpeg");
+        Catalog.GetString (@"Merge RAW+Jpeg as multiple versions of the same photo");
+        Catalog.GetString (@"Tools");
+
+        // ../src/Extensions/Tools/FSpot.Tools.RetroactiveRoll/Resources/RetroactiveRoll.addin.xml
+        Catalog.GetString (@"RetroactiveRoll");
+        Catalog.GetString (@"Retroactively assign old photos to import rolls");
+        Catalog.GetString (@"Tools");
+
+        // ../src/Extensions/Tools/FSpot.Tools.ScreensaverConfig/Resources/ScreensaverConfig.addin.xml
+        Catalog.GetString (@"Screensaver Configuration");
+        Catalog.GetString (@"Screensaver options");
+        Catalog.GetString (@"Tools");
+
+        // ../src/Extensions/Tools/FSpot.Tools.SyncCatalog/SyncCatalog.addin.xml
+        Catalog.GetString (@"SyncMetaData");
+        Catalog.GetString (@"Sync MetaData information of all catalog with Photos");
+        Catalog.GetString (@"Tools");
+
+        // ../src/Extensions/Transitions/FSpot.Transitions.Cover/Resources/CoverTransition.addin.xml
+        Catalog.GetString (@"Cover Transition");
+        Catalog.GetString (@"A Slideshow transition that covers images");
+        Catalog.GetString (@"Transitions");
+
+    }
+}
