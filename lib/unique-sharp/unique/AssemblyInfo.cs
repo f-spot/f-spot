ef03995a35ed263af83da0a23d01f6c2b7d265c9
Stephen Shaw <sshaw@decriptor.com>
2012-06-29 12:42:23 -0600

    [xbuild] Checking in the generated files
    Adding unique-sharp to the build process so that
    we can use xbuild.
    
    Since the .raw isn't going to change I'm generating
    and committing the files.
    
    Ultimately this is all going to go away with gtk-sharp3

--- lib/unique-sharp/unique/AssemblyInfo.cs
+++ lib/unique-sharp/unique/AssemblyInfo.cs
@@ -3,5 +3,4 @@ using System.Runtime.CompilerServices;
 
 [assembly:AssemblyVersion("1.0.0.0")]
 [assembly:AssemblyDelaySign(false)]
-[assembly:AssemblyKeyFile("unique-sharp.snk")]
 [assembly:GLib.IgnoreClassInitializers]
