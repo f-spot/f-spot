934f7c0e87ad9c073c4d686b5920109a9d05ef65
Stephen Shaw <sshaw@decriptor.com>
2012-06-29 13:22:02 -0600

    [xbuild] Added gio-sharp generated files
    This should really be revisited to see if we
    still need to carry this around with us.

--- lib/gio-sharp/gio/AssemblyInfo.cs
+++ lib/gio-sharp/gio/AssemblyInfo.cs
@@ -3,5 +3,4 @@ using System.Runtime.CompilerServices;
 
 [assembly:AssemblyVersion("2.14.0.0")]
 [assembly:AssemblyDelaySign(false)]
-[assembly:AssemblyKeyFile("./gtk-sharp.snk")]
 [assembly:GLib.IgnoreClassInitializers]
