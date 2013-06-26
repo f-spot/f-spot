804bd1fce0811ac5644797b60e5b7da8a2a8a0c1
Valentín Barros <valentin@sanva.net>
2013-06-26 09:21:52 +0200

    Implementation of a dummy widget and a basic addin, Faces Tool.
    - Implemented using code from the current tagging and editors classes.

---
+++ src/Clients/MainApp/FSpot/FaceStore.cs
@@ -0,0 +1,193 @@
+//
+// TagStore.cs
+//
+// Author:
+//   Ettore Perazzoli <ettore@src.gnome.org>
+//   Stephane Delcroix <stephane@delcroix.org>
+//   Larry Ewing <lewing@novell.com>
+//   Valentín Barros <valentin@sanva.net>
+//
+// Copyright (C) 2003-2009 Novell, Inc.
+// Copyright (C) 2003 Ettore Perazzoli
+// Copyright (C) 2007-2009 Stephane Delcroix
+// Copyright (C) 2004-2006 Larry Ewing
+// Copyright (C) 2013 Valentín Barros
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using Mono.Unix;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using FSpot;
+using FSpot.Core;
+using FSpot.Database;
+using FSpot.Jobs;
+using FSpot.Utils;
+
+using Hyena;
+using Hyena.Data.Sqlite;
+
+namespace FSpot {
+	public class InvalidFaceOperationException : InvalidOperationException {
+
+		public InvalidFaceOperationException (Face face, string message) : base (message)
+		{
+			Face = face;
+		}
+
+		public Face Face { get; set; }
+	}
+	
+	public class FaceStore : DbStore<Face> {
+
+		private const string STOCK_ICON_DB_PREFIX = "stock_icon:";
+
+		public Face GetFaceByName (string name)
+		{
+			foreach (Face face in this.item_cache.Values)
+				if (face.Name.ToLower () == name.ToLower ())
+					return face;
+	
+			return null;
+		}
+	
+		public Face GetFaceById (int id)
+		{
+			foreach (Face face in this.item_cache.Values)
+				if (face.Id == id)
+					return face;
+			return null;
+		}
+	
+		public Face [] GetFacesByNameStart (string s)
+		{
+			List <Face> faces = new List<Face> ();
+			foreach (Face face in this.item_cache.Values) {
+				if (face.Name.ToLower ().StartsWith (s.ToLower ()))
+					faces.Add (face);
+			}
+	
+			if (faces.Count == 0)
+				return null;
+	
+			faces.Sort (delegate (Face f1, Face f2) { return f1.CompareTo (f2); });
+	
+			return faces.ToArray ();
+		}
+
+		public Face [] GetAll ()
+		{
+			List <Face> faces = new List<Face> ();
+			foreach (Face face in item_cache.Values)
+				faces.Add (face);
+			return faces.ToArray ();
+		}
+
+		// In this store we keep all the items (i.e. the faces) in memory at all times.  This is
+		// mostly to make it a little bit faster.  We achieve this by passing "true" as the
+		// cache_is_immortal to our base class.
+		private void LoadAllFaces ()
+		{
+			//FIXME Fake method for testing purposes.
+			AddToCache (new Face (1, "Valentín Barros"));
+			AddToCache (new Face (2, "Stephen Shaw"));
+			AddToCache (new Face (3, "Sergio Casado"));
+			AddToCache (new Face (4, "Iago Barrera"));
+			AddToCache (new Face (5, "Santi Munín"));
+			AddToCache (new Face (6, "Nhoa Iglesias"));
+		}
+	
+		private void CreateTable ()
+		{
+			// TODO Implement it.
+		}
+		
+		// Constructor
+		public FaceStore (FSpotDatabaseConnection database, bool is_new)
+			: base (database, true)
+		{
+			if (is_new)
+				CreateTable ();
+			else
+				LoadAllFaces ();
+		}
+	
+		private uint InsertFaceIntoTable (string name)
+		{
+			// TODO Implement it.
+
+			return (uint) 0;
+		}
+	
+		public Face CreateFace (string name)
+		{
+			uint id = InsertFaceIntoTable (name);
+	
+			Face face = new Face (id, name);
+			face.IconWasCleared = true;
+	
+			AddToCache (face);
+			EmitAdded (face);
+	
+			return face;
+		}
+		
+		public override Face Get (uint id)
+		{
+		    return LookupInCache (id);
+		}
+
+		public override void Remove (Face face)
+		{
+			// TODO Implement it.
+		}
+
+		private string GetIconString (Face face)
+		{
+			if (face.Icon == null) {
+				if (face.IconWasCleared)
+					return String.Empty;
+				return null;
+			}
+			
+			return Convert.ToBase64String (GdkUtils.Serialize (face.Icon));
+		}
+	
+		public override void Commit (Face face)
+		{
+			Commit (face, false);
+		}
+	
+		public void Commit (Face face, bool update_xmp)
+		{
+			Commit (new Face[] {face}, update_xmp);
+		}
+	
+		public void Commit (Face [] faces, bool update_xmp)
+		{
+	
+			// TODO Implement it.
+		}
+	}
+}
