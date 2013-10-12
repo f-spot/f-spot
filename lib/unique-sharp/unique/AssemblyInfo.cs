7385e36dcd99f47fa059f52453d69fcb51941b64
Stephen Shaw <sshaw@decriptor.com>
2013-10-12 14:09:48 -0600

    Initial removal of all of the libunique-sharp code

--- lib/unique-sharp/unique/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly:AssemblyVersion("1.0.0.0")]
-[assembly:AssemblyDelaySign(false)]
-// GTK3:
-//[assembly:GLib.IgnoreClassInitializers]
