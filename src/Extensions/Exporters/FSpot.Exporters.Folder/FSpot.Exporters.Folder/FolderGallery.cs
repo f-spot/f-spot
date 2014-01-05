bda612cb4fb95ce41a7c2db03456dbbc08f657a0
Arun Persaud <arun@nubati.net>
2014-01-05 11:55:21 -0800

    updated FSF address

--- src/Extensions/Exporters/FSpot.Exporters.Folder/FSpot.Exporters.Folder/FolderGallery.cs
+++ src/Extensions/Exporters/FSpot.Exporters.Folder/FSpot.Exporters.Folder/FolderGallery.cs
@@ -45,8 +45,8 @@
  *
  * You should have received a copy of the GNU General Public
  * License along with this program; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301
  */
 
 //This should be used to export the selected pics to an original gallery
