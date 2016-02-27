c37c3d80c5e9ee3fb4c35c77ed25abf650c41b74
Daniel Köb <daniel.koeb@peony.at>
2016-02-27 11:39:47 +0100

    Implement latest XDG directory specification for thumbnails.

---
+++ src/Core/FSpot.Thumbnail.UnitTest/Mocks/EnvironmentMock.cs
@@ -0,0 +1,62 @@
+﻿//
+// EnvironmentMock.cs
+//
+// Author:
+//   Daniel Köb <daniel.koeb@peony.at>
+//
+// Copyright (C) 2016 Daniel Köb
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using FSpot.FileSystem;
+using Moq;
+
+namespace Mocks
+{
+	public class EnvironmentMock
+	{
+		readonly Mock<IEnvironment> mock;
+
+		public IEnvironment Object {
+			get {
+				return mock.Object;
+			}
+		}
+
+		public static IEnvironment Create (string user)
+		{
+			var environmentMock = new Mock<IEnvironment> ();
+			environmentMock.Setup (e => e.UserName).Returns (user);
+			return environmentMock.Object;
+		}
+
+		public EnvironmentMock (string variable, string value)
+		{
+			mock = new Mock<IEnvironment> ();
+			SetVariable (variable, value);
+		}
+
+		public void SetVariable (string variable, string value)
+		{
+			mock.Setup (m => m.GetEnvironmentVariable (variable)).Returns (value);
+		}
+	}
+}
---
+++ src/Core/FSpot.Thumbnail/XdgDirectoryService.cs
@@ -0,0 +1,92 @@
+﻿//
+// XdgDirectoryService.cs
+//
+// Author:
+//   Daniel Köb <daniel.koeb@peony.at>
+//
+// Copyright (C) 2016 Daniel Köb
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System.IO;
+using FSpot.FileSystem;
+using FSpot.Utils;
+
+namespace FSpot.Thumbnail
+{
+	/// <summary>
+	/// Implements some XDG directory specification
+	/// http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+	/// </summary>
+	class XdgDirectoryService : IXdgDirectoryService
+	{
+		readonly IFileSystem fileSystem;
+		readonly IEnvironment environment;
+		readonly object cacheLock = new object ();
+
+		string userCacheDir;
+
+		public XdgDirectoryService (IFileSystem fileSystem, IEnvironment environment)
+		{
+			this.fileSystem = fileSystem;
+			this.environment = environment;
+		}
+
+		/// <summary>
+		/// Gets the XDG user cache dir.
+		///
+		/// There is a single base directory relative to which user-specific non-essential (cached) data should be
+		/// written. This directory is defined by the environment variable $XDG_CACHE_HOME.
+		///
+		/// Re-implements glib's g_get_user_cache_dir
+		/// </summary>
+		/// <returns>The user cache dir.</returns>
+		public string GetUserCacheDir ()
+		{
+			lock (cacheLock) {
+				if (string.IsNullOrEmpty (userCacheDir)) {
+					userCacheDir = environment.GetEnvironmentVariable ("XDG_CACHE_HOME");
+
+					if (string.IsNullOrEmpty (userCacheDir)) {
+						string homeDir = environment.GetEnvironmentVariable ("HOME");
+
+						userCacheDir = string.IsNullOrEmpty (homeDir)
+							? Path.Combine (fileSystem.Path.GetTempPath (), environment.UserName, ".cache")
+							: Path.Combine (homeDir, ".cache");
+					}
+				}
+			}
+			return userCacheDir;
+		}
+
+		/// <summary>
+		/// Gets the thumbnails dir based on the user cache dir and the thumbnail size.
+		///
+		/// http://specifications.freedesktop.org/thumbnail-spec/thumbnail-spec-latest.html
+		/// </summary>
+		/// <returns>The thumbnails dir.</returns>
+		/// <param name="size">The thumbnail size to get the dir for.</param>
+		public string GetThumbnailsDir (ThumbnailSize size)
+		{
+			return Path.Combine (GetUserCacheDir (), "thumbnails", size == ThumbnailSize.Normal ? "normal" : "large");
+		}
+	}
+}
