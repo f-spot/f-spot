982f2317e3ef585d0e46c16e17c0588f5ab3bab7
Stephen Shaw <sshaw@decriptor.com>
2017-02-14 10:13:16 -0700

    Fixes a typo in the RepairDbDialog
    Fixes bgo#763597

--- src/Clients/MainApp/FSpot.UI.Dialog/RepairDbDialog.cs
+++ src/Clients/MainApp/FSpot.UI.Dialog/RepairDbDialog.cs
@@ -42,11 +42,11 @@ namespace FSpot.UI.Dialog
 {
 	public class RepairDbDialog : HigMessageDialog
 	{
-		public RepairDbDialog (System.Exception e, string backup_path, Window parent) :
+		public RepairDbDialog (Exception e, string backup_path, Window parent) :
 				base (parent, DialogFlags.DestroyWithParent, MessageType.Error, ButtonsType.Ok,
 				Catalog.GetString ("Error loading database."),
-				String.Format (Catalog.GetString ("F-Spot encountered an error while loading the photo database. " +
-						"The old database has be moved to {0} and a new database has been created."), backup_path))
+				string.Format (Catalog.GetString ("F-Spot encountered an error while loading the photo database. " +
+						"The old database has been moved to {0} and a new database has been created."), backup_path))
 		{
 			Log.Exception (e);
 			Run ();
