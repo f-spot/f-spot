29fded7ce5bde9580e580821be64a1c1cbc807b0
Stephen Shaw <sshaw@decriptor.com>
2020-10-11 22:22:56 -0600

    Look at dapper to replace hyena.sqlite

---
+++ src/Core/FSpot/Database/Dapper/Helper.cs
@@ -0,0 +1,25 @@
+//
+// Helper.cs
+//
+// Author:
+//   Stephen Shaw <sshaw@decriptor.com>>
+//
+// Copyright (C) 2020 Stephen Shaw
+//
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Dapper;
+
+namespace FSpot.Database.Dapper
+{
+	class Helper
+	{
+	}
+}
