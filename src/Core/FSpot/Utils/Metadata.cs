4781764a528bb199bc84f571c9c3e0323c630b5f
Stephen Shaw <sshaw@decriptor.com>
2020-10-11 18:32:14 -0600

    Fix TaglibSharpFileAbstraction unit test
    Make the Write stream read and write.
    
    Also, require the uri to be readonly and set through the ctor.

--- src/Core/FSpot/Utils/Metadata.cs
+++ src/Core/FSpot/Utils/Metadata.cs
@@ -7,25 +7,7 @@
 // Copyright (C) 2010 Novell, Inc.
 // Copyright (C) 2010 Ruben Vermeersch
 //
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 using System;
 using FSpot.FileSystem;
@@ -49,9 +31,9 @@ namespace FSpot.Utils
 			}
 
 			// Parse file
-			var res = new TagLibFileAbstraction { Uri = uri };
+			var res = new TagLibFileAbstraction (uri);
 			var sidecar_uri = GetSidecarUri (uri);
-			var sidecar_res = new TagLibFileAbstraction { Uri = sidecar_uri };
+			var sidecar_res = new TagLibFileAbstraction (sidecar_uri);
 
 			TagLib.Image.File file;
 			try {
@@ -82,7 +64,7 @@ namespace FSpot.Utils
 					Log.Warning ($"Metadata of file {photoUri} may be corrupt, refusing to write to it, falling back to XMP sidecar.");
 				}
 
-				var sidecar_res = new TagLibFileAbstraction () { Uri = GetSidecarUri (photoUri) };
+				var sidecar_res = new TagLibFileAbstraction (GetSidecarUri (photoUri));
 
 				metadata.SaveXmpSidecar (sidecar_res);
 			} else {
