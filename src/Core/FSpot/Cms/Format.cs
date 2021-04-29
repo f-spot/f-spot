74b5f03933c8abd7903ee6c1c5d862d6a8f8237c
Stephen Shaw <sshaw@decriptor.com>
2021-04-28 23:35:00 -0600

    Fix a few typos is Cms.Format
    Fixes #173
    https://github.com/f-spot/f-spot/issues/173

--- src/Core/FSpot/Cms/Format.cs
+++ src/Core/FSpot/Cms/Format.cs
@@ -7,41 +7,24 @@
 // Copyright (C) 2008 Novell, Inc.
 // Copyright (C) 2008 Stephane Delcroix
 //
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 namespace FSpot.Cms
 {
-	public enum Format : uint {
-		Rgb8  = 262169,
+	public enum Format : uint
+	{
+		Rgb8 = 262169,
 		Rgba8 = 262297,
 		Rgba8Planar = 266393,
-		Gbr8  = 263193,
+		Bgr8 = 263193,
 		Rgb16 = 262170,
 		Rgb16Planar = 266266,
 		Rgba16 = 262298,
-		Rgba16se = 264436,
+		Rgba16se = 264346,
 		Rgb16se = 264218,
 		Lab8 = 655385,
-	        Lab16 = 655386,
-		Xyz16 = 589858,
+		Lab16 = 655386,
+		Xyz16 = 589850,
 		Yxy16 = 917530
 	}
 }
